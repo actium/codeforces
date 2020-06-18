78a241dc3258efb37af401fd9a78e788be7895a0
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-06-18 20:42:15 +0700

    Add solution for problem 1285A

---
+++ 1200/80/1285a.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+void answer(size_t v)
+{
+    std::cout << v << '\n';
+}
+
+void solve(const std::string& s)
+{
+    // s.count('L') + (1 for x=0) + s.count('R') = s.length() + 1
+    answer(s.length() + 1);
+}
+
+int main()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::string s;
+    std::cin >> s;
+
+    solve(s);
+
+    return 0;
+}
+
