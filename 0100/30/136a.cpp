1873542ff788dcb0e774a0663858578b0f4f9061
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-06-18 11:16:07 +0700

    Add solution for problem 136A

---
+++ 0100/30/136a.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        std::cin >> a;
+}
+
+template <typename T>
+void answer(const std::vector<T>& v)
+{
+    const char* separator = "";
+    for (const T a : v) {
+        std::cout << separator << a;
+        separator = " ";
+    }
+}
+
+void solve(const std::vector<size_t>& p)
+{
+    const size_t n = p.size();
+
+    std::vector<size_t> s(n);
+    for (size_t i = 0; i < n; ++i)
+        s[p[i]-1] = i+1;
+
+    answer(s);
+}
+
+int main()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<size_t> p(n);
+    std::cin >> p;
+
+    solve(p);
+
+    return 0;
+}
+
