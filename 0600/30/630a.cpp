40329eaecbc3849e934698886c4ec7f6da70db07
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-06-18 20:35:27 +0700

    Add solution for problem 630A

---
+++ 0600/30/630a.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+void answer(unsigned v)
+{
+    std::cout << v << '\n';
+}
+
+int main()
+{
+    unsigned long long n;
+    std::cin >> n;
+
+    answer(25);
+
+    return 0;
+}
+
