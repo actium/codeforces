73098d82d4aa41e487c6f76514ec1ebbde15db7b
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-06-18 21:09:45 +0700

    Add solution for problem 1312A

---
+++ 1300/10/1312a.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(size_t n, size_t m)
+{
+    answer(n % m == 0);
+}
+
+void test_case()
+{
+    size_t n, m;
+    std::cin >> n >> m;
+
+    solve(n, m);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
+
