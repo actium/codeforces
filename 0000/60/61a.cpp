bb9f77550fc3e5a7caecf27830f6723b3a7ffc3e
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-06-18 12:13:08 +0700

    Add solution for problem 61A

---
+++ 0000/60/61a.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+void answer(const std::string& v)
+{
+    std::cout << v << '\n';
+}
+
+std::string& solve(std::string& n1, const std::string& n2)
+{
+    const size_t n = n1.length();
+    for (size_t i = 0; i < n; ++i)
+        n1[i] = '0' + (n1[i] ^ n2[i]);
+
+    answer(n1);
+}
+
+int main()
+{
+    std::string n1, n2;
+    std::cin >> n1 >> n2;
+
+    solve(n1, n2);
+
+    return 0;
+}
+
