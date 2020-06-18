33dfb52ddcf3a3431b2c5c2dc6e6afefdd5663b5
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-06-18 20:44:32 +0700

    Add solution for problem 1234A

---
+++ 1200/30/1234a.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned v)
+{
+    std::cout << v << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    const size_t n = a.size();
+
+    unsigned sum = 0;
+    for (size_t i = 0; i < n; ++i)
+        sum += a[i];
+
+    answer((sum + n - 1) / n);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
+
