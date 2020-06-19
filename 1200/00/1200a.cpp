fdae3749dd2f6496cbe0c27ef1b5d1985d95295d
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-06-20 00:10:52 +0700

    Add solution for problem 1200A

---
+++ 1200/00/1200a.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+
+void answer(const std::string& v)
+{
+    std::cout << v << '\n';
+}
+
+std::string solve(const std::string& s)
+{
+    std::string t(10, '0');
+
+    for (const char e : s) {
+        if (e == 'L') {
+            const size_t p = t.find('0');
+            //assert(p != std::string::npos);
+            t[p] = '1';
+            continue;
+        }
+        if (e == 'R') {
+            const size_t p = t.rfind('0');
+            //assert(p != std::string::npos);
+            t[p] = '1';
+            continue;
+        }
+
+        t[e - '0'] = '0';
+    }
+
+    answer(t);
+}
+
+int main()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::string s;
+    std::cin >> s;
+
+    solve(s);
+
+    return 0;
+}
+
