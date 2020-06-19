5ac838c12ba4d6247ac984f1da90b9cff4402d8c
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-06-19 21:18:18 +0700

    Add solution for problem 1041A

---
+++ 1000/40/1041a.cpp
@@ -0,0 +1,38 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(size_t v)
+{
+    std::cout << v << '\n';
+}
+
+void solve(std::vector<unsigned>& a)
+{
+    std::sort(a.begin(), a.end());
+
+    answer(a.back() - a.front() + 1 - a.size());
+}
+
+int main()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+
+    return 0;
+}
+
