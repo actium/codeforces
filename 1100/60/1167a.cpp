25611405e7af0f376fab186e3a178129c2aa52dd
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-06-19 22:45:17 +0700

    Add solution for problem 1167A

---
+++ 1100/60/1167a.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(const std::string& s)
+{
+    const size_t p = s.find('8');
+    if (p == std::string::npos)
+        return answer(false);
+
+    answer(s.length() - p >= 11);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::string s;
+    std::cin >> s;
+
+    solve(s);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
+
