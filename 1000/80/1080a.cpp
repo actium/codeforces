2dfb6120031f6c5e203111e43321904c0a8d1994
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-06-20 00:40:18 +0700

    Add solution for problem 1080A

---
+++ 1000/80/1080a.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+void answer(size_t v)
+{
+    std::cout << v << '\n';
+}
+
+size_t solve(size_t n, size_t k)
+{
+    const size_t r = n * 2;
+    const size_t g = n * 5;
+    const size_t b = n * 8;
+
+    answer((r + k - 1) / k + (g + k - 1) / k + (b + k - 1) / k);
+}
+
+int main()
+{
+    size_t n, k;
+    std::cin >> n >> k;
+
+    solve(n, k);
+
+    return 0;
+}
+
