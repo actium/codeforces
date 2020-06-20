d27874b716f4033398b1d1480f31955b4fdf3e48
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-06-20 10:13:12 +0700

    Add solution for problem 946A

---
+++ 0900/40/946a.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(int v)
+{
+    std::cout << v << '\n';
+}
+
+void solve(const std::vector<int>& a)
+{
+    int d = 0;
+    for (const int x : a)
+        d += abs(x);
+
+    answer(d);
+}
+
+int main()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<int> a(n);
+    std::cin >> a;
+
+    solve(a);
+
+    return 0;
+}
+
