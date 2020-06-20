eaa7e8e1b1b934011d85307df1cde56b6ed942fc
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-06-20 12:29:17 +0700

    Add solution for problem 818A

---
+++ 0800/10/818a.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using integer = long long;
+
+void answer(integer x, integer y, integer z)
+{
+    std::cout << x << ' ' << y << ' ' << z << '\n';
+}
+
+const char* solve(integer n, integer k)
+{
+    const integer x = (n / 2) / (1 + k);
+
+    answer(x, x * k, n - x - x * k);
+}
+
+int main()
+{
+    integer n, k;
+    std::cin >> n >> k;
+
+    solve(n, k);
+
+    return 0;
+}
+
