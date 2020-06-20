c4ab8573756fc26b752cdf97ebf1df7d00b4e34d
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-06-20 12:45:19 +0700

    Add solution for problem 802G

---
+++ 0800/00/802g.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+bool solve(const std::string& s)
+{
+    const char p[] = "heidi";
+
+    size_t i = 0;
+    for (const char c : s) {
+        if (i < 5 && c == p[i])
+            ++i;
+    }
+
+    answer(i == 5);
+}
+
+int main()
+{
+    std::string s;
+    std::cin >> s;
+
+    solve(s);
+
+    return 0;
+}
+
