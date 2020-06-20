715f4035d57a189bee9bc53e203f133e72944028
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-06-20 11:48:29 +0700

    Add solution for problem 748A

---
+++ 0700/40/748a.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+void answer(size_t x, size_t y, char z)
+{
+    std::cout << 1 + x << ' ' << 1 + y << ' ' << z << '\n';
+}
+
+const char* solve(size_t n, size_t m, size_t k)
+{
+    const size_t x = (k - 1) / (2 * m);
+    const size_t y = (k - 1) % (2 * m);
+
+    answer(x, y / 2, (y % 2 == 0 ? 'L' : 'R'));
+}
+
+int main()
+{
+    size_t n, m, k;
+    std::cin >> n >> m >> k;
+
+    solve(n, m, k);
+
+    return 0;
+}
+
