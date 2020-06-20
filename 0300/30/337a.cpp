3692b0fe31bc5557143ace9e0efa5bff0070ec87
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-06-20 12:57:58 +0700

    Add solution for problem 337A

---
+++ 0300/30/337a.cpp
@@ -0,0 +1,42 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(size_t v)
+{
+    std::cout << v << '\n';
+}
+
+size_t solve(std::vector<size_t>& f, size_t n)
+{
+    std::sort(f.begin(), f.end());
+
+    size_t min_delta = f[n-1] - f[0];
+    for (size_t left = 1, right = n, m = f.size(); right < m; ++left, ++right)
+        min_delta = std::min(min_delta, f[right] - f[left]);
+
+    answer(min_delta);
+}
+
+int main()
+{
+    size_t n, m;
+    std::cin >> n >> m;
+
+    std::vector<size_t> f(m);
+    std::cin >> f;
+
+    solve(f, n);
+
+    return 0;
+}
+
