732994e9f0241ebddd98b0837978406ad270acef
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-06-20 12:59:39 +0700

    Add solution for problem 405A

---
+++ 0400/00/405a.cpp
@@ -0,0 +1,43 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(const std::vector<int>& v)
+{
+    const char* separator = "";
+    for (const int x : v) {
+        std::cout << separator << x;
+        separator = " ";
+    }
+    std::cout << '\n';
+}
+
+void solve(std::vector<int>& a)
+{
+    std::sort(a.begin(), a.end());
+
+    answer(a);
+}
+
+int main()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<int> a(n);
+    std::cin >> a;
+
+    solve(a);
+
+    return 0;
+}
+
