ff28ec76e332d50ce76088d2820f7cbc92dcd382
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-06-20 10:26:44 +0700

    Add solution for problem 937A

---
+++ 0900/30/937a.cpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(size_t v)
+{
+    std::cout << v << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    bool z[600] = {};
+    for (const unsigned x : a)
+        z[x-1] = true;
+
+    answer(std::count(z + 0, z + 600, true));
+}
+
+int main()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+
+    return 0;
+}
+
