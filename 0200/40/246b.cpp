1dc841e160a8caf26bd211332c9673b2b3c0b338
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-06-26 20:22:49 +0700

    Add solution for problem 246B

---
+++ 0200/40/246b.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(size_t v)
+{
+    std::cout << v << '\n';
+}
+
+void solve(const std::vector<int>& a)
+{
+    const size_t n = a.size();
+
+    int sum = 0;
+    for (const int x : a)
+        sum += x;
+
+    answer(n - (sum % n != 0));
+}
+
+int main()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<int> a(n);
+    std::cin >> a;
+
+    solve(a);
+
+    return 0;
+}
+
