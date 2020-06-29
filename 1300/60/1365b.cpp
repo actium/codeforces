07bdb07d57e037b380ae8e0a2dc615b5706d6b70
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-06-29 14:36:15 +0700

    Add solution for problem 1365B

---
+++ 1300/60/1365b.cpp
@@ -0,0 +1,58 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "No", "Yes" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(const std::vector<unsigned>& a, const std::vector<unsigned>& b)
+{
+    const size_t k = std::count(b.cbegin(), b.cend(), 1);
+    if (k != 0 && k != b.size())
+        return answer(true);
+
+    for (size_t i = 0; i < a.size() - 1; ++i) {
+        if (a[i] > a[i+1])
+            return answer(false);
+    }
+
+    answer(true);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    std::vector<unsigned> b(n);
+    std::cin >> b;
+
+    solve(a, b);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
+
