33f90d230d95fb8cc26fa757954d372d0f8d1d7a
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-06-29 14:25:38 +0700

    Add solution for problem 1369A

---
+++ 1300/60/1369a.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(size_t n)
+{
+    answer(n % 4 == 0);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    solve(n);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
+
