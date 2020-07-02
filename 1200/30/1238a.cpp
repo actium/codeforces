121e9b364c21ea9d4fa40e9e13bdc16374df1d71
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-07-02 11:28:52 +0700

    Add solution for problem 1238A

---
+++ 1200/30/1238a.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using integer = unsigned long long;
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(integer x, integer y)
+{
+    const integer d = x - y;
+
+    answer(d > 1);
+}
+
+void test_case()
+{
+    integer x, y;
+    std::cin >> x >> y;
+
+    solve(x, y);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
+
