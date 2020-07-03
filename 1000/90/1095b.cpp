9f171dc9f3018ad01a062b269ada0f6542185119
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-07-03 21:56:41 +0700

    Add solution for problem 1095B

---
+++ 1000/90/1095b.cpp
@@ -0,0 +1,39 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned v)
+{
+    std::cout << v << '\n';
+}
+
+void solve(std::vector<unsigned>& a)
+{
+    std::sort(a.begin(), a.end());
+
+    const size_t n = a.size();
+    answer(std::min(a[n-2] - a[0], a[n-1] - a[1]));
+}
+
+int main()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+
+    return 0;
+}
+
