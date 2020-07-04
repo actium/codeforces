5b7f74451356219bc90f588eec96dcc199089aca
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-07-04 20:55:57 +0700

    Add solution for problem 1197A

---
+++ 1100/90/1197a.cpp
@@ -0,0 +1,53 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned v)
+{
+    std::cout << v << '\n';
+}
+
+void solve(std::vector<unsigned>& a)
+{
+    const size_t n = a.size();
+    if (n == 2)
+        return answer(0);
+
+    std::sort(a.begin(), a.end());
+
+    const size_t k = a[n-2] - 1;
+
+    answer(std::min(k, n - 2));
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
+
