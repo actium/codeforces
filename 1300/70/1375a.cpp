e5c3fe2c0d1681de8c29902257e32c6e9926683a
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-07-05 11:13:40 +0700

    Add solution for problem 1375A

---
+++ 1300/70/1375a.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(const std::vector<int>& v)
+{
+    const char* separator = "";
+    for (const int x : v) {
+        std::cout << separator << x;
+        separator = " ";
+    }
+    std::cout << '\n';
+}
+
+void solve(std::vector<int>& a)
+{
+    const size_t n = a.size();
+
+    for (size_t i = 0; i < n; ++i) {
+        if (i % 2 == 0)
+            a[i] = abs(a[i]);
+        else
+            a[i] = -abs(a[i]);
+    }
+
+    answer(a);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<int> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
+
