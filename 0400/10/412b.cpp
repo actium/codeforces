a434b7e61e8245abf411ce4723bd251b38c2d2c3
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-07-06 22:34:55 +0700

    Add solution for problem 412B

---
+++ 0400/10/412b.cpp
@@ -0,0 +1,38 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned v)
+{
+    std::cout << v << '\n';
+}
+
+void solve(std::vector<unsigned>& a, size_t k)
+{
+    std::sort(a.begin(), a.end(), std::greater<unsigned>());
+
+    answer(a[k-1]);
+}
+
+int main()
+{
+    size_t n, k;
+    std::cin >> n >> k;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a, k);
+
+    return 0;
+}
+
