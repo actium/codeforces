3344d5b3261d06681f0509ab3c98a7c9568668a3
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-07-11 17:47:18 +0700

    Add solution for problem 465A

---
+++ 0400/60/465a.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+void answer(unsigned v)
+{
+    std::cout << v << '\n';
+}
+
+void solve(const std::string& s)
+{
+    const size_t n = s.length();
+
+    size_t k = 0;
+    while (k < n && s[k] == '1')
+        ++k;
+
+    answer(k + (k != n));
+}
+
+int main()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::string s;
+    std::cin >> s;
+
+    solve(s);
+
+    return 0;
+}
+
