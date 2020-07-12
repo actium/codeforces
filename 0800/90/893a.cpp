33812f6f4ed49c20bb5b7b9ea38a2983eed9c816
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-07-12 11:08:47 +0700

    Add solution for problem 893A

---
+++ 0800/90/893a.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    unsigned spectator = 3;
+    for (const unsigned x : a) {
+        if (x == spectator)
+            return answer(false);
+
+        spectator = 6 - spectator - x;
+    }
+
+    answer(true);
+}
+
+int main()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+
+    return 0;
+}
+
