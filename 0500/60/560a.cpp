cc7dd6f6e066856bc1cc8da148817fa6f5acf3ee
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-07-16 20:44:47 +0700

    Add solution for problem 560A

---
+++ 0500/60/560a.cpp
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(int v)
+{
+    std::cout << v << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    answer(std::find(a.cbegin(), a.cend(), 1) == a.cend() ? 1 : -1);
+}
+
+int main()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+
+    return 0;
+}
+
