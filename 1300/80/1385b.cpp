20dc339ce5ba463923109e700e5a197de77fb52c
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-07-20 11:10:56 +0700

    Add solution for problem 1385B

---
+++ 1300/80/1385b.cpp
@@ -0,0 +1,56 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(const std::vector<unsigned>& v)
+{
+    const char* separator = "";
+    for (const unsigned x : v) {
+        std::cout << separator << x;
+        separator = " ";
+    }
+    std::cout << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    std::vector<unsigned> p;
+    for (const unsigned x : a) {
+        if (std::find(p.cbegin(), p.cend(), x) == p.cend())
+            p.push_back(x);
+    }
+
+    answer(p);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(2 * n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
+
