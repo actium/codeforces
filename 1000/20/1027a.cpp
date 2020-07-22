835130b893a993e8a0f2f61cb7dc298e37dd6c59
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-07-22 21:07:09 +0700

    Add solution for problem 1027A

---
+++ 1000/20/1027a.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(const std::string& s)
+{
+    const size_t n = s.length();
+
+    for (size_t i = 0; i < n/2; ++i) {
+        const unsigned d = abs(s[i] - s[n-i-1]);
+        if (d != 0 && d != 2)
+            return answer(false);
+    }
+
+    answer(true);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::string s;
+    std::cin >> s;
+
+    solve(s);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
+
