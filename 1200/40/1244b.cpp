134367cfdaa91299baaa5b01c665383534a56523
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-07-22 21:19:52 +0700

    Add solution for problem 1244B

---
+++ 1200/40/1244b.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+
+void answer(unsigned v)
+{
+    std::cout << v << '\n';
+}
+
+void solve(const std::string& s)
+{
+    const size_t n = s.length();
+
+    const size_t a = s.find('1');
+    if (a == std::string::npos)
+        return answer(n);
+
+    const size_t b = s.rfind('1');
+    answer(2 * std::max(n - a, b + 1));
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::string s;
+    std::cin >> s;
+
+    solve(s);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
+
