69fa482131284874a49918352efc30f288238c0b
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-07-24 13:53:38 +0700

    Add solution for problem 1390A

---
+++ 1300/90/1390a.cpp
@@ -0,0 +1,43 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned v)
+{
+    std::cout << v << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    const size_t n = a.size();
+
+    unsigned f[3] = {};
+    for (const unsigned x : a)
+        ++f[x-1];
+
+    const unsigned k = *std::max_element(std::begin(f), std::end(f));
+    answer(n - k);
+}
+
+int main()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+
+    return 0;
+}
+
