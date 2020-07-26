2b4f103bc32f8cc5d1cbbe0e0283c22173aab05c
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-07-26 10:38:29 +0700

    Add solution for problem 691A

---
+++ 0600/90/691a.cpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    const size_t n = a.size();
+
+    const size_t k = std::count(a.cbegin(), a.cend(), 0);
+    answer(n == 1 ? k == 0 : k == 1);
+}
+
+int main()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+
+    return 0;
+}
+
