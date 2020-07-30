8f5b8b4b12ed5662735e5ace39dbe30f56b8893e
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-07-30 11:58:14 +0700

    Add solution for problem 1140A

---
+++ 1100/40/1140a.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned v)
+{
+    std::cout << v << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    const size_t n = a.size();
+
+    unsigned k = 0;
+    for (size_t i = 1; i <= n; ++i) {
+        for (unsigned x = a[i-1]; x > i; ++i)
+            x = std::max(x, a[i]);
+
+        ++k;
+    }
+
+    answer(k);
+}
+
+int main()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+
+    return 0;
+}
+
