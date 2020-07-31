9d6059272cb598ccda8e05d7f8a7cc30cc3065ad
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-07-31 10:16:39 +0700

    Add solution for problem 849A

---
+++ 0800/40/849a.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "No", "Yes" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    answer(a.size() % 2 == 1 && a.front() % 2 == 1 && a.back() % 2 == 1);
+}
+
+int main()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+
+    return 0;
+}
+
