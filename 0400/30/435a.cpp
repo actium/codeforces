e5745d429d308345c1aa4ca2737b3cd7e43004b5
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-08-01 01:44:16 +0700

    Add solution for problem 435A

---
+++ 0400/30/435a.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned v)
+{
+    std::cout << v << '\n';
+}
+
+void solve(const std::vector<unsigned>& a, unsigned m)
+{
+    unsigned c = m, k = 0;
+    for (const unsigned x : a) {
+        if (c + x <= m)
+            c += x;
+        else
+            ++k, c = x;
+    }
+
+    answer(k);
+}
+
+int main()
+{
+    size_t n;
+    std::cin >> n;
+
+    unsigned m;
+    std::cin >> m;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a, m);
+
+    return 0;
+}
+
