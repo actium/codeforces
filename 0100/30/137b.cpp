308fae2ab5035ad79800f6fc89ea38c94ed517e1
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-08-01 02:31:33 +0700

    Add solution for problem 137B

---
+++ 0100/30/137b.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <set>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned v)
+{
+    std::cout << v << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    const size_t n = a.size();
+
+    unsigned k = 0;
+
+    std::set<unsigned> s;
+    for (const unsigned x : a) {
+        if (x <= n && s.count(x) == 0)
+            s.insert(x);
+        else
+            ++k;
+    }
+
+    answer(k);
+}
+
+int main()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+
+    return 0;
+}
+
