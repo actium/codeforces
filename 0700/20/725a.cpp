eb06a3fbcd14878708a09dee3c171313eec47abb
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-08-02 12:25:06 +0700

    Add solution for problem 725A

---
+++ 0700/20/725a.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+void answer(unsigned v)
+{
+    std::cout << v << '\n';
+}
+
+void solve(const std::string& s)
+{
+    const size_t n = s.length();
+
+    const size_t x = s.find('>');
+    const size_t y = s.rfind('<');
+
+    answer(n - (x < y ? y - x + 1 : 0));
+}
+
+int main()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::string s;
+    std::cin >> s;
+
+    solve(s);
+
+    return 0;
+}
+
