2afb37649419b4e23a431db2a83cb03a33f3a787
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-08-02 20:16:44 +0700

    Add solution for problem 637A

---
+++ 0600/30/637a.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <map>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned v)
+{
+    std::cout << v << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    unsigned top = ~0u;
+
+    std::map<unsigned, unsigned> c;
+    for (const unsigned x : a) {
+        const unsigned k = ++c[x];
+        if (top == ~0u || k > c[top])
+            top = x;
+    }
+
+    answer(top);
+}
+
+int main()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+
+    return 0;
+}
+
