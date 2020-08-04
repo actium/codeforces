cfad891eefc5c5c7b5024609dde6293b60fc5066
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-08-04 15:46:23 +0700

    Add solution for problem 1252A

---
+++ 1200/50/1252a.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(const std::vector<unsigned>& v)
+{
+    const char* separator = "";
+    for (const unsigned x : v) {
+        std::cout << separator << x;
+        separator = " ";
+    }
+    std::cout << '\n';
+}
+
+void solve(std::vector<unsigned>& a)
+{
+    const unsigned n = a.size();
+
+    for (unsigned& x : a)
+        x = n - x + 1;
+
+    answer(a);
+}
+
+int main()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+
+    return 0;
+}
+
