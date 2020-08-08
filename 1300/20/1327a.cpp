22edf219ad06e61899d205fac0adac761ef7deae
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-08-08 20:01:31 +0700

    Add solution for problem 1327A

---
+++ 1300/20/1327a.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(unsigned n, unsigned k)
+{
+    answer(n % 2 == k % 2 && n >= 1ull * k * k);
+}
+
+void test_case()
+{
+    unsigned n, k;
+    std::cin >> n >> k;
+
+    solve(n, k);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
+
