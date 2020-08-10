2c1f61435a6865d86d22f60d6a690b0350a158cb
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-08-10 21:35:35 +0700

    Add solution for problem 1213B

---
+++ 1200/10/1213b.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned v)
+{
+    std::cout << v << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    unsigned k = 0;
+    for (auto i = a.rbegin(), j = i; i != a.rend(); ++i) {
+        if (*i > *j)
+            ++k;
+
+        if (*i < *j)
+            j = i;
+    }
+
+    answer(k);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
+
