947f7f334512c3ef8fb213a320168dce940e5047
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-08-10 20:36:44 +0700

    Add solution for problem 1391A

---
+++ 1300/90/1391a.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <numeric>
+#include <vector>
+
+void answer(const std::vector<unsigned>& v)
+{
+    const char* separator = "";
+    for (const unsigned x : v) {
+        std::cout << separator << x;
+        separator = " ";
+    }
+    std::cout << '\n';
+}
+
+void solve(unsigned n)
+{
+    std::vector<unsigned> p(n);
+    std::iota(p.begin(), p.end(), 1);
+
+    answer(p);
+}
+
+void test_case()
+{
+    unsigned n;
+    std::cin >> n;
+
+    solve(n);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
+
