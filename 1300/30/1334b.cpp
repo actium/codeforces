71407d25cea3b45833faf4d99f32d909a6253e29
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-08-11 22:49:21 +0700

    Add solution for problem 1334B

---
+++ 1300/30/1334b.cpp
@@ -0,0 +1,55 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned v)
+{
+    std::cout << v << '\n';
+}
+
+void solve(std::vector<unsigned>& a, unsigned x)
+{
+    const size_t n = a.size();
+
+    std::sort(a.begin(), a.end(), std::greater<unsigned>());
+
+    size_t i = 0;
+    for (unsigned long long d = 0; i < n && a[i] + d >= x; d = a[i] + d - x, ++i);
+
+    answer(i);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    unsigned x;
+    std::cin >> x;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a, x);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
+
