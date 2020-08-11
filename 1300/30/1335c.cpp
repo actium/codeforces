f780550b0361afe3aa2ebd4b826082e334a4b1c0
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-08-11 23:18:02 +0700

    Add solution for problem 1335C

---
+++ 1300/30/1335c.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <map>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned v)
+{
+    std::cout << v << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    std::map<unsigned, unsigned> f;
+    for (const unsigned x : a)
+        ++f[x];
+
+    unsigned x = 0;
+    for (const auto& e : f) {
+        if (e.second > x)
+            x = e.second;
+    }
+
+    const unsigned k = f.size();
+    if (x == k)
+        --x;
+
+    answer(std::min(x, k));
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
+
