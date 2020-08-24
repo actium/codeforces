6236f75b38dae28ecdddba2a6535240386e2ad35
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-08-24 17:21:28 +0700

    Add solution for problem 1392A

---
+++ 1300/90/1392a.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned v)
+{
+    std::cout << v << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    const size_t n = a.size();
+
+    for (size_t i = 1; i < n; ++i) {
+        if (a[i] != a.front())
+            return answer(1);
+    }
+
+    answer(n);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
+
