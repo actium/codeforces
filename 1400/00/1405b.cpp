c738f314bb2bc274d7e0b826bacf4a870638bc1a
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-09-06 20:23:39 +0300

    Add solution for problem 1405B

---
+++ 1400/00/1405b.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned long long v)
+{
+    std::cout << v << '\n';
+}
+
+void solve(const std::vector<int>& a)
+{
+    long long s = 0, k = 0;
+    for (auto it = a.crbegin(); it != a.crend(); ++it) {
+        s += *it;
+        k = std::max(k, s);
+    }
+
+    answer(k);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<int> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
+
