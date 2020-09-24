4a5d6e0411feb9e55478cb6424aa631d2b0bee11
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-09-24 21:44:26 +0300

    Add solution for problem 1420B

---
+++ 1400/20/1420b.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned long long v)
+{
+    std::cout << v << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    unsigned long long k = 0;
+
+    unsigned c[33] = {};
+    for (const unsigned x : a) {
+        const size_t i = __builtin_clz(x);
+        k += c[i]++;
+    }
+
+    answer(k);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
+
