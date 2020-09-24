c139edad32ab2d56f322f788338f79717fd3838f
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-09-24 22:04:22 +0300

    Add solution for problem 1420C1

---
+++ 1400/20/1420c1.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <numeric>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned long long v)
+{
+    std::cout << v << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    const size_t n = a.size();
+
+    unsigned long long p = a[0];
+    for (size_t i = 1; i < n; ++i) {
+        if (a[i] > a[i-1])
+            p += a[i] - a[i-1];
+    }
+
+    answer(p);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    unsigned q;
+    std::cin >> q;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
+
