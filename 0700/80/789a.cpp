7e91712588c48c123323bee46212626531541e8d
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-10-08 22:32:19 +0300

    Add solution for problem 789A

---
+++ 0700/80/789a.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned v)
+{
+    std::cout << v << '\n';
+}
+
+void solve(const std::vector<unsigned>& a, unsigned k)
+{
+    unsigned c = 0;
+    for (const unsigned x : a)
+        c += (x + k - 1) / k;
+
+    answer((c + 1) / 2);
+}
+
+int main()
+{
+    size_t n;
+    std::cin >> n;
+
+    unsigned k;
+    std::cin >> k;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a, k);
+
+    return 0;
+}
+
