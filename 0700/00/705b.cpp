e77882589de97b7777623143f0d7ea675564ee19
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-10-09 21:32:02 +0300

    Add solution for problem 705B

---
+++ 0700/00/705b.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned v)
+{
+    std::cout << v << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    unsigned s = 0;
+    for (const unsigned x : a) {
+        s += x - 1;
+
+        answer(2 - s % 2);
+    }
+}
+
+int main()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+
+    return 0;
+}
+
