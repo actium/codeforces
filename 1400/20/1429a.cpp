0f7fb68e61025c394ce6c5737a3a5f745be49bde
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-10-10 17:42:18 +0300

    Add solution for problem 1429A

---
+++ 1400/20/1429a.cpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(size_t v)
+{
+    std::cout << v << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    unsigned f[3] = {};
+    for (const unsigned x : a)
+        ++f[x-1];
+
+    answer(std::min({ f[0] + f[1], f[0] + f[2], f[1] + f[2] }));
+}
+
+int main()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+
+    return 0;
+}
+
