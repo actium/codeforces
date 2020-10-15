3ed4ed8f70fc8d82468b0639f50da06c184c292b
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-10-16 01:08:45 +0300

    Add solution for problem 731B

---
+++ 0700/30/731b.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(std::vector<unsigned>& a)
+{
+    const size_t n = a.size();
+
+    unsigned s = 0;
+    for (size_t i = 0; i < n && (a[i] != 0 || s % 2 == 0); ++i)
+        s += a[i];
+
+    answer(s % 2 == 0);
+}
+
+int main()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+
+    return 0;
+}
+
