d71e9916850149da6b7bac53e6d5e81ea73567ce
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-10-19 16:57:09 +0300

    Add solution for problem 461A

---
+++ 0400/60/461a.cpp
@@ -0,0 +1,44 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned long long v)
+{
+    std::cout << v << '\n';
+}
+
+void solve(std::vector<unsigned>& a)
+{
+    const size_t n = a.size();
+
+    std::sort(a.begin(), a.end());
+
+    unsigned long long s = 0;
+    for (size_t i = 0; i < n; ++i)
+        s += (2ull + i) * a[i];
+
+    answer(s - a.back());
+}
+
+int main()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+
+    return 0;
+}
+
