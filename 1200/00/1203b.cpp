7d0a8e2cb74fbcd69936b4444a9f645a359521a9
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-10-19 20:13:28 +0300

    Add solution for problem 1203B

---
+++ 1200/00/1203b.cpp
@@ -0,0 +1,57 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(std::vector<unsigned>& a)
+{
+    const size_t n = a.size();
+    const size_t k = n / 2;
+
+    std::sort(a.begin(), a.end());
+
+    const unsigned s = a.front() * a.back();
+    for (size_t i = 0; i < k; ++i) {
+        if (a[i] * a[n-1-i] != s || a[i] != a[i^1])
+            return answer(false);
+    }
+
+    answer(true);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(4 * n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
+
