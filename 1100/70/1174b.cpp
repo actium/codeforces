f6a0095220fdd20bb08fe37dd953100d4f0b2ce8
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-10-21 14:14:43 +0300

    Add solution for problem 1174B

---
+++ 1100/70/1174b.cpp
@@ -0,0 +1,45 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(const std::vector<unsigned>& v)
+{
+    const char* separator = "";
+    for (const unsigned x : v) {
+        std::cout << separator << x;
+        separator = " ";
+    }
+    std::cout << '\n';
+}
+
+void solve(std::vector<unsigned>& a)
+{
+    const size_t k = std::count_if(a.cbegin(), a.cend(), [](unsigned x) { return x % 2 == 0; });
+    if (k != 0 && k != a.size())
+        std::sort(a.begin(), a.end());
+
+    answer(a);
+}
+
+int main()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+
+    return 0;
+}
+
