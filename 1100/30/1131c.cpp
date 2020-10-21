b303da73832ceb474b216b9bfb1bc4930f5b60b0
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-10-21 14:03:56 +0300

    Add solution for problem 1131C

---
+++ 1100/30/1131c.cpp
@@ -0,0 +1,53 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(const std::vector<unsigned>& v)
+{
+    const char* separator = "";
+    for (const unsigned x : v) {
+        std::cout << separator << x;
+        separator = " ";
+    }
+    std::cout << '\n';
+}
+
+void solve(std::vector<unsigned>& a)
+{
+    const size_t n = a.size();
+
+    std::sort(a.begin(), a.end());
+
+    std::vector<unsigned> b(n);
+    for (size_t i = 0; i < n; ++i) {
+        if (i % 2 == 0)
+            b[(n+i)/2] = a[i];
+        else
+            b[(n-1-i)/2] = a[i];
+    }
+
+    answer(b);
+}
+
+int main()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+
+    return 0;
+}
+
