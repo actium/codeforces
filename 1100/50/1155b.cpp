0a4eb1d66ef34163ff0c757732837a0117dbb575
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-10-23 22:25:34 +0300

    Add solution for problem 1155B

---
+++ 1100/50/1155b.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(const std::string& s)
+{
+    const size_t n = s.length();
+
+    unsigned k = 0;
+    for (size_t i = 0; i + 11 <= n; ++i)
+        k += (s[i] == '8');
+
+    answer(k > (n - 11) / 2);
+}
+
+int main()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::string s;
+    std::cin >> s;
+
+    solve(s);
+
+    return 0;
+}
+
