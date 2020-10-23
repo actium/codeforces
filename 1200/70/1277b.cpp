a27fabc16e4151b6c7d0a4af59b7d397d30f7dc8
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-10-23 15:13:02 +0300

    Add solution for problem 1277B

---
+++ 1200/70/1277b.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <map>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned v)
+{
+    std::cout << v << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    std::map<unsigned, unsigned> f;
+    for (const unsigned x : a)
+        ++f[x];
+
+    unsigned k = 0;
+    while (!f.empty()) {
+        const auto it = std::prev(f.end());
+        if (it->first % 2 == 0) {
+            f[it->first / 2] += it->second;
+            ++k;
+        }
+        f.erase(it);
+    }
+
+    answer(k);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
+
