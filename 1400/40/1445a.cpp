cc66b7dbea9e280b28b99a2f8e831649734136a7
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-11-01 16:41:57 +0300

    Add solution for problem 1445A

---
+++ 1400/40/1445a.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(const std::vector<unsigned>& a, const std::vector<unsigned>& b, unsigned x)
+{
+    const size_t n = a.size();
+
+    for (size_t i = 0; i < n; ++i) {
+        if (a[i] + b[n-1-i] > x)
+            return answer(false);
+    }
+
+    answer(true);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    unsigned x;
+    std::cin >> x;
+
+    std::vector<unsigned> a(n), b(n);
+    std::cin >> a >> b;
+
+    solve(a, b, x);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
+
