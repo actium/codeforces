5b0693b54d02b591ef6f706e7251417ef0f8581b
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-11-13 19:28:14 +0300

    Add solution for problem 1438B

---
+++ 1400/30/1438b.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <set>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(const std::vector<unsigned>& b)
+{
+    const std::set<unsigned> c(b.cbegin(), b.cend());
+
+    answer(c.size() < b.size());
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> b(n);
+    std::cin >> b;
+
+    solve(b);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
+
