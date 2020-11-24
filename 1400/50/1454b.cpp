7c2068cf419b4c695916f1da1192c5437c28b6af
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-11-24 20:59:20 +0300

    Add solution for problem 1454B

---
+++ 1400/50/1454b.cpp
@@ -0,0 +1,60 @@
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(size_t v)
+{
+    std::cout << 1+v << '\n';
+}
+
+void no_answer()
+{
+    std::cout << -1 << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    std::map<unsigned, unsigned> f;
+    for (const unsigned x : a)
+        ++f[x];
+
+    for (const auto& p : f) {
+        if (p.second == 1)
+            return answer(std::find(a.cbegin(), a.cend(), p.first) - a.cbegin());
+    }
+
+    no_answer();
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
+
