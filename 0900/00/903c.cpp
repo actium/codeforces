565c5c437b78017b82bc4c76e01e4d8728f98aeb
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-12-04 22:52:37 +0300

    Add solution for problem 903C

---
+++ 0900/00/903c.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <map>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned v)
+{
+    std::cout << v << '\n';
+}
+
+void solve(std::vector<unsigned>& a)
+{
+    std::map<unsigned, unsigned> f;
+    for (const unsigned x : a)
+        ++f[x];
+
+    unsigned k = 0;
+    for (const auto& x : f)
+        k = std::max(k, x.second);
+
+    answer(k);
+}
+
+int main()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+
+    return 0;
+}
+
