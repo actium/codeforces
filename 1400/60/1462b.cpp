6e4b795652fcf689ce77f53a153c3934cd607728
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-12-15 19:57:45 +0300

    Add solution for problem 1462B

---
+++ 1400/60/1462b.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(std::string& s)
+{
+    std::string t = "2020";
+    while (!t.empty() && s.back() == t.back()) {
+        s.pop_back();
+        t.pop_back();
+    }
+
+    answer(s.substr(0, t.size()) == t);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::string s;
+    std::cin >> s;
+
+    solve(s);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
+
