0be541082c3540aa55a1505b70328cd814ff990b
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-12-17 21:36:00 +0300

    Add solution for problem 1463B

---
+++ 1400/60/1463b.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <numeric>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(const std::vector<unsigned>& v)
+{
+    const char* separator = "";
+    for (const unsigned x : v) {
+        std::cout << separator << x;
+        separator = " ";
+    }
+    std::cout << '\n';
+}
+
+void solve(std::vector<unsigned>& a)
+{
+    const size_t n = a.size();
+
+    unsigned long long s[2] = {};
+    for (size_t i = 0; i < n; ++i)
+        s[i % 2] += a[i];
+
+    for (size_t i = s[0] > s[1]; i < n; i += 2)
+        a[i] = 1;
+
+    answer(a);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
+
