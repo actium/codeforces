072b142999a22b2e79c359e99fcafd248e075131
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-12-18 02:24:00 +0300

    Add solution for problem 31A

---
+++ 0000/30/31a.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(size_t x, size_t y, size_t z)
+{
+    std::cout << 1+x << ' ' << 1+y << ' ' << 1+z << '\n';
+}
+
+void no_answer()
+{
+    std::cout << -1 << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    const size_t n = a.size();
+
+    for (size_t i = 0; i < n; ++i) {
+        for (size_t j = 0; j < n; ++j) {
+            for (size_t k = 0; k < n; ++k) {
+                if (a[i] == a[j] + a[k] && k != i && k != j && j != i)
+                    return answer(i, j, k);
+            }
+        }
+    }
+
+    no_answer();
+}
+
+int main()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+
+    return 0;
+}
+
