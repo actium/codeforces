6f883d76665c4d75387c4eb80811ee448bf9a658
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-12-20 21:07:32 +0300

    Add solution for problem 1465A

---
+++ 1400/60/1465a.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "No", "Yes" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(const std::string& s)
+{
+    unsigned l = s.length(), r = 0;
+    for (auto it = s.rbegin(); it != s.rend() && *it == ')'; ++it)
+        --l, ++r;
+
+    answer(l < r);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::string s;
+    std::cin >> s;
+
+    solve(s);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
+
