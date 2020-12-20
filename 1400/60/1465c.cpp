83f9917151e1ce070d7e18d4b88f84007d36a241
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-12-20 21:36:27 +0300

    Add solution for problem 1465C

---
+++ 1400/60/1465c.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+
+template <typename T, typename U>
+std::istream& operator >>(std::istream& input, std::pair<T, U>& v)
+{
+    return input >> v.first >> v.second;
+}
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned v)
+{
+    std::cout << v << '\n';
+}
+
+void solve(size_t n, const std::vector<std::pair<unsigned, unsigned>>& p)
+{
+    unsigned k = p.size();
+
+    std::vector<unsigned> r(1+n);
+    for (const auto [x, y] : p) {
+        if (x != y)
+            r[x] = y;
+        else
+            --k;
+    }
+
+    for (unsigned x = 1; x <= n; ++x) {
+        if (r[x] != 0) {
+            unsigned y = x;
+            while (r[y] != 0)
+                y = std::exchange(r[y], 0);
+
+            if (x == y)
+                ++k;
+        }
+    }
+
+    answer(k);
+}
+
+void test_case()
+{
+    size_t n, m;
+    std::cin >> n >> m;
+
+    std::vector<std::pair<unsigned, unsigned>> p(m);
+    std::cin >> p;
+
+    solve(n, p);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
+
