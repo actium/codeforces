67edea69ca5985478193fd0aaac88971f9d460ca
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-12-25 20:14:11 +0300

    Add solution for problem 134A

---
+++ 0100/30/134a.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(const std::vector<size_t>& v)
+{
+    std::cout << v.size();
+
+    const char* separator = "\n";
+    for (const size_t x : v) {
+        std::cout << separator << 1+x;
+        separator = " ";
+    }
+    std::cout << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    const size_t n = a.size();
+
+    unsigned s = 0;
+    for (const unsigned x : a)
+        s += x;
+
+    std::vector<size_t> p;
+    for (size_t i = 0; i < n; ++i) {
+        if (a[i] * n == s)
+            p.push_back(i);
+    }
+
+    answer(p);
+}
+
+int main()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+
+    return 0;
+}
+
