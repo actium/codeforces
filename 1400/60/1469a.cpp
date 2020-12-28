a1552655a69dfd40e0dedf49e2b8198c4ab8eb42
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-12-28 20:50:59 +0300

    Add solution for problem 1469A

---
+++ 1400/60/1469a.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(const std::string& s)
+{
+    answer(s.length() % 2 == 0 && s.front() != ')' && s.back() != '(');
+}
+
+void test_case()
+{
+    std::string s;
+    std::cin >> s;
+
+    solve(s);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
+
