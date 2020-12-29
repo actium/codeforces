edbf9146327b132d2d77e35e622adc7cc0db662f
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-12-29 19:15:41 +0300

    Add solution for problem 802M

---
+++ 0800/00/802m.cpp
@@ -0,0 +1,42 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned v)
+{
+    std::cout << v << '\n';
+}
+
+void solve(std::vector<unsigned>& a, size_t k)
+{
+    std::sort(a.begin(), a.end());
+
+    unsigned s = 0;
+    for (size_t i = 0; i < k; ++i)
+        s += a[i];
+
+    answer(s);
+}
+
+int main()
+{
+    size_t n, k;
+    std::cin >> n >> k;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a, k);
+
+    return 0;
+}
+
