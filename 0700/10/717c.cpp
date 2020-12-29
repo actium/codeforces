bc9523c98deb423d50e76541840ab8ec8394f141
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2020-12-29 22:29:56 +0300

    Add solution for problem 717C

---
+++ 0700/10/717c.cpp
@@ -0,0 +1,46 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned v)
+{
+    std::cout << v << '\n';
+}
+
+void solve(std::vector<unsigned>& a)
+{
+    const size_t n = a.size();
+
+    std::sort(a.begin(), a.end());
+
+    unsigned s = 0;
+    for (size_t i = 0; i < n; ++i) {
+        s += 1ull * a[i] * a[n-1-i] % 10007;
+        s %= 10007;
+    }
+
+    answer(s);
+}
+
+int main()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+
+    return 0;
+}
+
