a5859ed9ce21906f82bc09633e363de3cb524aa6
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2021-01-01 14:49:11 +0300

    Add solution for problem 1468E

---
+++ 1400/60/1468e.cpp
@@ -0,0 +1,44 @@
+#include <algorithm>
+#include <array>
+#include <iostream>
+
+template <typename T, size_t N>
+std::istream& operator >>(std::istream& input, std::array<T, N>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned v)
+{
+    std::cout << v << '\n';
+}
+
+void solve(std::array<unsigned, 4>& a)
+{
+    std::sort(a.begin(), a.end());
+
+    answer(a[0] * a[2]);
+}
+
+void test_case()
+{
+    std::array<unsigned, 4> a;
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
+
