4333f48dc35e3bd8afa7665b086fbd8f979d5b0a
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2021-01-04 19:55:20 +0300

    Add solution for problem 1472C

---
+++ 1400/70/1472c.cpp
@@ -0,0 +1,55 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned v)
+{
+    std::cout << v << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    const size_t n = a.size();
+
+    std::vector<unsigned> dp(n);
+    for (size_t i = n-1; ~i; --i) {
+        dp[i] = a[i];
+
+        if (i + a[i] < n)
+            dp[i] += dp[i+a[i]];
+    }
+
+    answer(*std::max_element(dp.cbegin(), dp.cend()));
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
+
