66f94dc64cc2ba5de10c7edf8bbde081c0697fd6
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2021-01-05 00:08:22 +0300

    Add solution for problem 1260B

---
+++ 1200/60/1260b.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(unsigned a, unsigned b)
+{
+    if (a < b)
+        std::swap(a, b);
+
+    answer((a + b) % 3 == 0 && a <= 2 * b);
+}
+
+void test_case()
+{
+    unsigned a, b;
+    std::cin >> a >> b;
+
+    solve(a, b);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
+
