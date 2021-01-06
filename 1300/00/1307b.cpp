00b77455e6d7c75971882e14ea4a0130176c7a01
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2021-01-06 11:38:04 +0300

    Add solution for problem 1307B

---
+++ 1300/00/1307b.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned v)
+{
+    std::cout << v << '\n';
+}
+
+void solve(const std::vector<unsigned>& a, unsigned x)
+{
+    unsigned b = 0;
+    for (const unsigned q : a) {
+        if (q == x)
+            return answer(1);
+
+        b = std::max(b, q);
+    }
+
+    answer(b > x ? 2 : (x + b - 1) / b);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    unsigned x;
+    std::cin >> x;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a, x);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
+
