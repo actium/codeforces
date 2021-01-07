3651ee5e8ca0de6bb0b96a1f93acb501d7de1fa3
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2021-01-08 01:42:17 +0300

    Add solution for problem 220A

---
+++ 0200/20/220a.cpp
@@ -0,0 +1,46 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    const size_t n = a.size();
+
+    std::vector<unsigned> b = a;
+    std::sort(b.begin(), b.end());
+
+    unsigned k = 0;
+    for (size_t i = 0; i < n; ++i)
+        k += (a[i] != b[i]);
+
+    answer(k < 3);
+}
+
+int main()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+
+    return 0;
+}
+
