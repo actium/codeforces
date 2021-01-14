8b70b6da5983dbd7d332ac5fc2c207188f1272d6
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2021-01-15 02:24:12 +0300

    Add solution for problem 135A

---
+++ 0100/30/135a.cpp
@@ -0,0 +1,46 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(const std::vector<unsigned>& v)
+{
+    const char* separator = "";
+    for (const unsigned x : v) {
+        std::cout << separator << x;
+        separator = " ";
+    }
+    std::cout << '\n';
+}
+
+void solve(std::vector<unsigned>& a)
+{
+    unsigned& x = *std::max_element(a.begin(), a.end());
+    x = (x + 1) / x;
+
+    std::sort(a.begin(), a.end());
+
+    answer(a);
+}
+
+int main()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+
+    return 0;
+}
+
