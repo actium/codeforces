00a153ff8ee5e713e9912205e5390b5e45dd5776
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2021-01-16 19:33:02 +0300

    Add solution for problem 1159B

---
+++ 1100/50/1159b.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned v)
+{
+    std::cout << v << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    const size_t n = a.size();
+
+    unsigned k = ~0u;
+    for (size_t i = 0; i < n; ++i)
+        k = std::min<unsigned>(k, a[i] / std::max(i, n-1-i));
+
+    answer(k);
+}
+
+int main()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+
+    return 0;
+}
+
