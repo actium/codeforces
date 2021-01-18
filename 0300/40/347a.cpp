2bbcc399bc3a5470919217432be1a8538d497bab
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2021-01-18 17:05:41 +0300

    Add solution for problem 347A

---
+++ 0300/40/347a.cpp
@@ -0,0 +1,45 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(const std::vector<int>& v)
+{
+    const char* separator = "";
+    for (const int x : v) {
+        std::cout << separator << x;
+        separator = " ";
+    }
+    std::cout << '\n';
+}
+
+void solve(std::vector<int>& a)
+{
+    std::sort(a.begin(), a.end());
+
+    std::swap(a.front(), a.back());
+
+    answer(a);
+}
+
+int main()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<int> a(n);
+    std::cin >> a;
+
+    solve(a);
+
+    return 0;
+}
+
