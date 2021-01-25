77dc0bf40d312000bdce685258f1b5af9d73a377
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2021-01-25 20:45:05 +0300

    Add solution for problem 1475B

---
+++ 1400/70/1475b.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(unsigned n)
+{
+    answer(n % 2020 <= n / 2020);
+}
+
+void test_case()
+{
+    unsigned n;
+    std::cin >> n;
+
+    solve(n);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
+
