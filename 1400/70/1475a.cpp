5addaa83fa7f016968b229f5b6015d266d3c2edc
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2021-01-25 20:43:33 +0300

    Add solution for problem 1475A

---
+++ 1400/70/1475a.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using integer = unsigned long long;
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(integer n)
+{
+    answer((n & (n - 1)) != 0);
+}
+
+void test_case()
+{
+    integer n;
+    std::cin >> n;
+
+    solve(n);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
+
