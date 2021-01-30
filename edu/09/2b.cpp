5652d770279fa0aa49d93b34914de2f02da6eafc
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2021-01-30 22:14:36 +0300

    Add solution for problem edu:9/2B

---
+++ edu/09/2b.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(int v)
+{
+    std::cout << v << '\n';
+}
+
+using integer = unsigned long long;
+
+void solve(const std::vector<unsigned>& a, integer b)
+{
+    const size_t n = a.size();
+
+    integer s = 0, t = ~0u;
+    for (size_t i = 0, j = 0; j < n; ++j) {
+        for (s += a[j]; s >= b; s -= a[i++])
+            t = std::min<unsigned>(t, j - i + 1);
+    }
+
+    answer(t);
+}
+
+int main()
+{
+    size_t n;
+    std::cin >> n;
+
+    integer b;
+    std::cin >> b;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a, b);
+
+    return 0;
+}
+
