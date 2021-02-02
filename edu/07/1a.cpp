299476853a24c2fb03d2a573c028eb11d94a0ce5
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2021-02-02 17:23:28 +0300

    Add solution for problem edu:7/1A

---
+++ edu/07/1a.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <utility>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+struct Query {
+    std::string t;
+    unsigned u;
+    unsigned v;
+};
+
+std::istream& operator >>(std::istream& input, Query& v)
+{
+    return input >> v.t >> v.u >> v.v;
+}
+
+class ComponentSet {
+public:
+    explicit ComponentSet(size_t size);
+
+    size_t operator [](size_t x) noexcept;
+
+    void merge(size_t x, size_t y) noexcept;
+
+private:
+    std::vector<size_t> roots_;
+    std::vector<size_t> sizes_;
+
+}; // class ComponentSet
+
+inline ComponentSet::ComponentSet(size_t size) :
+    roots_(size), sizes_(size, 1)
+{
+    for (size_t i = 0; i < size; ++i)
+        roots_[i] = i;
+}
+
+inline size_t ComponentSet::operator [](size_t x) noexcept
+{
+    size_t root = x;
+    while (root != roots_[root])
+        root = roots_[root];
+
+    while (roots_[x] != root)
+        x = std::exchange(roots_[x], root);
+
+    return root;
+}
+
+inline void ComponentSet::merge(size_t x, size_t y) noexcept
+{
+    size_t rx = operator [](x), ry = operator [](y);
+    if (rx == ry)
+        return;
+
+    if (sizes_[rx] < sizes_[ry])
+        std::swap(rx, ry);
+
+    roots_[ry] = rx;
+    sizes_[rx] += sizes_[ry];
+}
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(size_t n, const std::vector<Query>& q)
+{
+    ComponentSet cs(1+n);
+
+    for (const auto [t, u, v] : q) {
+        if (t == "union")
+            cs.merge(u, v);
+        else
+            answer(cs[u] == cs[v]);
+    }
+}
+
+int main()
+{
+    size_t n, m;
+    std::cin >> n >> m;
+
+    std::vector<Query> q(m);
+    std::cin >> q;
+
+    solve(n, q);
+
+    return 0;
+}
+
