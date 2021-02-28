f37af526daffcb0e769126d5267ffa2065ed450d
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2021-02-28 21:11:43 +0300

    Add solution for problem 1487A

---
+++ 1400/80/1487a.cpp
@@ -0,0 +1,51 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned v)
+{
+    std::cout << v << '\n';
+}
+
+void solve(std::vector<unsigned>& a)
+{
+    std::sort(a.begin(), a.end());
+
+    unsigned k = 0;
+    for (const unsigned q : a)
+        k += (q > a[0]);
+
+    answer(k);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
+
