b2d0a3abd6eb27d7a1ba11af5631af028cecca5d
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2021-12-10 11:33:37 +0300

    Add solution for problem 1579A

---
+++ 1500/70/1579a.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(const std::string& s)
+{
+    unsigned f[3] = {};
+    for (const char c : s)
+        ++f[c - 'A'];
+
+    answer(f[0] + f[2] == f[1]);
+}
+
+void test_case()
+{
+    std::string s;
+    std::cin >> s;
+
+    solve(s);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
