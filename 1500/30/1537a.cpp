69868808bd932eca2dcd04b9ef47ff7f34bb60d0
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2021-12-11 12:33:05 +0300

    Add solution for problem 1537A

---
+++ 1500/30/1537a.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<int>& a)
+{
+    const int n = a.size();
+
+    int s = 0;
+    for (const auto x : a)
+        s += x;
+
+    answer(s < n ? 1 : s - n);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<int> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
