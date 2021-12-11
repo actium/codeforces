28270331aa682392b0fc1cdb72008e65597c41e4
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2021-12-11 12:39:10 +0300

    Add solution for problem 1529A

---
+++ 1500/20/1529a.cpp
@@ -0,0 +1,47 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    const auto it = std::min_element(a.cbegin(), a.cend());
+    const auto k = std::count(a.cbegin(), a.cend(), *it);
+
+    answer(a.size() - k);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
