b2161e8b2bd63a376a43bfeecceb9b053999dfdb
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2021-12-12 10:56:27 +0300

    Add solution for problem 1554A

---
+++ 1500/50/1554a.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+
+using integer = unsigned long long;
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(integer x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    const size_t n = a.size();
+
+    integer x = 0;
+    for (size_t i = 1; i < n; ++i)
+        x = std::max(x, 1ull * a[i-1] * a[i]);
+
+    answer(x);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
