742f274c685fd69a5d68d5e11423f48581e01cce
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2021-12-15 13:24:58 +0300

    Add solution for problem 1573A

---
+++ 1500/70/1573a.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::string& s)
+{
+    const size_t n = s.length();
+
+    unsigned k = 0;
+    for (size_t i = 0; i < n; ++i) {
+        if (s[i] > '0')
+            k += (s[i] - '0') + (i < n - 1);
+    }
+
+    answer(k);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::string s;
+    std::cin >> s;
+
+    solve(s);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
