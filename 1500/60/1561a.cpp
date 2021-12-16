d8d3b397212eafa7bd0b5ddd0e41d481e439674c
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2021-12-16 15:56:03 +0300

    Add solution for problem 1561A

---
+++ 1500/60/1561a.cpp
@@ -0,0 +1,54 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(std::vector<unsigned>& a)
+{
+    const size_t n = a.size();
+
+    unsigned k = 0;
+    for ( ; !std::is_sorted(a.begin(), a.end()); ++k) {
+        for (size_t j = 1 + k % 2; j < n; j += 2) {
+            if (a[j] < a[j-1])
+                std::swap(a[j-1], a[j]);
+        }
+    }
+
+    answer(k);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
