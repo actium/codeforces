be407d306058d3c9135de5e9dca0ecbb689c10fb
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2021-12-19 21:41:41 +0300

    Add solution for problem 1501B

---
+++ 1500/00/1501b.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(const std::vector<bool>& v)
+{
+    const char* separator = "";
+    for (const bool x : v) {
+        std::cout << separator << x;
+        separator = " ";
+    }
+    std::cout << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    const size_t n = a.size();
+
+    std::vector<bool> b(n);
+    for (int i = n - 1, j = n; i >= 0; --i) {
+        j = std::min<int>(j, i - a[i]);
+        b[i] = (i > j);
+    }
+
+    answer(b);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
