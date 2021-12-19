4e87ce4d5ad992a0049b3c27141b43091fd9db18
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2021-12-20 00:30:42 +0300

    Add solution for problem 1609A

---
+++ 1600/00/1609a.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+
+using integer = unsigned long long;
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(integer x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    integer s = 0, b = 0, p = 0;
+    for (integer x : a) {
+        while (x % 2 == 0)
+            x /= 2, ++p;
+
+        s += x;
+        b = std::max(b, x);
+    }
+
+    answer(s - b + (b << p));
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
