34bc1cd2490bbecce925b628926443638600c017
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2021-12-20 13:24:28 +0300

    Add solution for problem 1585B

---
+++ 1500/80/1585b.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    unsigned x = a.back(), k = 0;
+    for (auto it = a.rbegin(); it != a.rend(); ++it) {
+        if (*it > x) {
+            x = *it;
+            ++k;
+        }
+    }
+
+    answer(k);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
