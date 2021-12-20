93ff45bcb4bbe3e1e7ba4dc59b673ddbe532adef
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2021-12-20 22:05:05 +0300

    Add solution for problem 1620A

---
+++ 1600/20/1620a.cpp
@@ -0,0 +1,33 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(const std::string& s)
+{
+    answer(std::count(s.cbegin(), s.cend(), 'N') != 1);
+}
+
+void test_case()
+{
+    std::string s;
+    std::cin >> s;
+
+    solve(s);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
