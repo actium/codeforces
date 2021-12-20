8b6fb6cfdd8af89a913c74d224cf02034fe5e4c8
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2021-12-20 19:10:30 +0300

    Add solution for problem 1499B

---
+++ 1400/90/1499b.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(const std::string& s)
+{
+    const size_t x = s.find("11");
+    answer(x == std::string::npos || s.find("00", x + 2) == std::string::npos);
+}
+
+void test_case()
+{
+    std::string s;
+    std::cin >> s;
+
+    solve(s);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
