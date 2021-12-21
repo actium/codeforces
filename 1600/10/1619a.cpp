55e7f43285c7e04902a1ffa81609d7cb3deaec06
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2021-12-21 23:23:40 +0300

    Add solution for problem 1619A

---
+++ 1600/10/1619a.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(const std::string& s)
+{
+    const size_t n = s.length();
+
+    answer(s.substr(0, n / 2) == s.substr(n / 2));
+}
+
+void test_case()
+{
+    std::string s;
+    std::cin >> s;
+
+    solve(s);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
