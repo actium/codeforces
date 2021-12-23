bb51b2810abe3076c9da30c83560adbe7e8312be
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2021-12-23 11:56:27 +0300

    Add solution for problem 1611C

---
+++ 1600/10/1611c.cpp
@@ -0,0 +1,59 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(const std::vector<unsigned>& v)
+{
+    const char* separator = "";
+    for (const unsigned x : v) {
+        std::cout << separator << x;
+        separator = " ";
+    }
+    std::cout << '\n';
+}
+
+void no_answer()
+{
+    std::cout << -1 << '\n';
+}
+
+void solve(std::vector<unsigned>& a)
+{
+    const auto x = *std::max_element(a.cbegin(), a.cend());
+    if (x != a.front() && x != a.back())
+        return no_answer();
+
+    std::reverse(a.begin(), a.end());
+    answer(a);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
