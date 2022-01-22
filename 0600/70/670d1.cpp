0d34b09a539f87986c4d0ed5cb738a13f6239f1f
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2022-01-22 17:56:40 +0300

    Add solution for problem 670D1

---
+++ 0600/70/670d1.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& a, const std::vector<unsigned>& b, unsigned k)
+{
+    const size_t n = a.size();
+
+    const auto f = [&](unsigned x) {
+        unsigned q = k;
+        for (size_t i = 0; i < n; ++i) {
+            const unsigned r = a[i] * x;
+            if (b[i] >= r)
+                continue;
+
+            const unsigned d = r - b[i];
+            if (d > q)
+                return false;
+
+            q -= d;
+        }
+        return true;
+    };
+
+    unsigned lb = 0, ub = 2001;
+    while (lb + 1 < ub) {
+        const unsigned mid = lb + (ub - lb) / 2;
+        (f(mid) ? lb : ub) = mid;
+    }
+
+    answer(lb);
+}
+
+int main()
+{
+    size_t n;
+    std::cin >> n;
+
+    unsigned k;
+    std::cin >> k;
+
+    std::vector<unsigned> a(n), b(n);
+    std::cin >> a >> b;
+
+    solve(a, b, k);
+
+    return 0;
+}
