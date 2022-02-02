27f25750eb6657eb9ff423476d72ba1478dfb20a
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2022-02-02 21:42:52 +0300

    Add solution for problem 1629A

---
+++ 1600/20/1629a.cpp
@@ -0,0 +1,60 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& a, const std::vector<unsigned>& b, unsigned k)
+{
+    const size_t n = a.size();
+
+    std::vector<size_t> o(n);
+    for (size_t i = 0; i < n; ++i)
+        o[i] = i;
+
+    std::sort(o.begin(), o.end(), [&](size_t x, size_t y) { return a[x] < a[y]; });
+
+    for (const size_t i : o) {
+        if (a[i] <= k)
+            k += b[i];
+    }
+
+    answer(k);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    unsigned k;
+    std::cin >> k;
+
+    std::vector<unsigned> a(n), b(n);
+    std::cin >> a >> b;
+
+    solve(a, b, k);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
