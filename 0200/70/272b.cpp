a95d3e52424c169f851e6c70b3a193731c3e6a4c
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2022-02-09 08:32:37 +0300

    Add solution for problem 272B

---
+++ 0200/70/272b.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+using integer = unsigned long long;
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(integer x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    integer k = 0;
+
+    unsigned f[32] = {};
+    for (const unsigned x : a)
+        k += f[__builtin_popcount(x)]++;
+
+    answer(k);
+}
+
+int main()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+
+    return 0;
+}
