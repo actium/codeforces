b32b3d96ac613f2bdf3fb462548dff949f606107
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2022-02-14 00:56:44 +0300

    Add solution for problem 270B

---
+++ 0200/70/270b.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    const size_t n = a.size();
+
+    for (size_t i = n-1; i > 0; --i) {
+        if (a[i-1] > a[i])
+            return answer(i);
+    }
+
+    answer(0);
+}
+
+int main()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+
+    return 0;
+}
