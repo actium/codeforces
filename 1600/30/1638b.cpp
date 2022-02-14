b5ae8c24bb24382dc050976d4ac35cfe95f6f9e4
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2022-02-14 22:14:01 +0300

    Add solution for problem 1638B

---
+++ 1600/30/1638b.cpp
@@ -0,0 +1,49 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    std::vector<unsigned> s[2];
+    for (const unsigned x : a)
+        s[x % 2].push_back(x);
+
+    answer(std::is_sorted(s[0].begin(), s[0].end()) && std::is_sorted(s[1].begin(), s[1].end()));
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
