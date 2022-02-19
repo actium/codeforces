85abe42ee0c6e14e984a625bd18e75a653cb5299
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2022-02-19 12:16:54 +0300

    Add solution for problem 1638C

---
+++ 1600/30/1638c.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& p)
+{
+    const size_t n = p.size();
+
+    unsigned k = 0, x = 0;
+    for (size_t i = 0; i < n; ++i) {
+        x = std::max(x, p[i]);
+        k += (i+1 == x);
+    }
+
+    answer(k);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> p(n);
+    std::cin >> p;
+
+    solve(p);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
