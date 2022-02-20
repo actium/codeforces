c669659f18d1813052c3e034f98fb2704aa60883
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2022-02-20 15:13:01 +0300

    Add solution for problem 1201B

---
+++ 1200/00/1201b.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    unsigned long long s = 0, b = 0;
+    for (const unsigned x : a) {
+        s += x;
+        b = std::max<unsigned>(b, x);
+    }
+
+    answer(s % 2 == 0 && s >= 2 * b);
+}
+
+int main()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+
+    return 0;
+}
+
