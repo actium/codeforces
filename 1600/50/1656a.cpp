1e45fd249401c513c22cf8f1c83e4a4a08cc19ca
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2022-03-30 08:49:02 +0300

    Add solution for problem 1656A

---
+++ 1600/50/1656a.cpp
@@ -0,0 +1,46 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(size_t x, size_t y)
+{
+    std::cout << 1+x << ' ' << 1+y << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    const auto r = std::minmax_element(a.cbegin(), a.cend());
+
+    answer(r.first - a.cbegin(), r.second - a.cbegin());
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
