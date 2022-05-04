f37f84889e7cbbc39c4742f036048b8421ec93da
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2022-05-04 15:48:27 +0300

    Add solution for problem 1674B

---
+++ 1600/70/1674b.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::string& s)
+{
+    const auto index = [](char c) {
+        return c - 'a';
+    };
+
+    answer(index(s[0]) * 25 + index(s[1]) + (s[1] < s[0]));
+}
+
+void test_case()
+{
+    std::string s;
+    std::cin >> s;
+
+    solve(s);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
