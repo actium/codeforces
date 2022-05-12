bbe23c91c5019ae83309698137dfb8e4fe9a72dd
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2022-05-12 12:51:22 +0300

    Add solution for problem 1676A

---
+++ 1600/70/1676a.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(const std::string& s)
+{
+    answer(s[0] + s[1] + s[2] == s[3] + s[4] + s[5]);
+}
+
+void test_case()
+{
+    std::string s;
+    std::cin >> s;
+
+    solve(s);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
