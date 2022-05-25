dbacc98ca51d152b49485ab41c09a46fbf1c2353
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2022-05-25 15:43:22 +0300

    Add solution for problem 1681A

---
+++ 1600/80/1681a.cpp
@@ -0,0 +1,55 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "Alice", "Bob" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(const std::vector<unsigned>& a, const std::vector<unsigned>& b)
+{
+    const unsigned x = *std::max_element(a.cbegin(), a.cend());
+    const unsigned y = *std::max_element(b.cbegin(), b.cend());
+
+    answer(x < y);
+    answer(x <= y);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    size_t m;
+    std::cin >> m;
+
+    std::vector<unsigned> b(m);
+    std::cin >> b;
+
+    solve(a, b);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
