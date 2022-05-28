78b8270f44c7c194e362925874116c499ee0eb48
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2022-05-28 10:28:36 +0300

    Add solution for problem 1682A

---
+++ 1600/80/1682a.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::string& s)
+{
+    const size_t n = s.length();
+
+    int lb = n / 2;
+    while (lb >= 0 && s[lb] == s[n/2])
+        --lb;
+
+    answer(n - 2 * (lb + 1));
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::string s;
+    std::cin >> s;
+
+    solve(s);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
