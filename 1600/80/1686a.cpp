06fc9b0f5420e61b1c4e26b8350f5c69f56ec3b7
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2022-05-29 09:26:46 +0300

    Add solution for problem 1686A

---
+++ 1600/80/1686a.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    const size_t n = a.size();
+
+    unsigned s = 0;
+    for (const unsigned x : a)
+        s += x;
+
+    for (const unsigned x : a) {
+        if (s - x == x * (n - 1))
+            return answer(true);
+    }
+
+    answer(false);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
