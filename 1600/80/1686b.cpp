31a880a9574317e4e7e4ce858538434563bdd07d
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2022-05-30 13:42:29 +0300

    Add solution for problem 1686B

---
+++ 1600/80/1686b.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& p)
+{
+    const size_t n = p.size();
+
+    unsigned k = 0;
+    for (size_t i = 1; i < n; ++i) {
+        if (p[i] < p[i-1]) {
+            ++k;
+            ++i;
+        }
+    }
+
+    answer(k);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> p(n);
+    std::cin >> p;
+
+    solve(p);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
