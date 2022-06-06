435dd565b48ecb9d99c0031b11ed8507cd8b76ea
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2022-06-06 08:51:15 +0300

    Add solution for problem 1688B

---
+++ 1600/80/1688b.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    unsigned f[2] = {}, q = 32;
+    for (const unsigned x : a) {
+        ++f[x % 2];
+        q = std::min<unsigned>(q, __builtin_ctz(x));
+    }
+
+    answer(f[1] != 0 ? f[0] : q + f[0] - 1);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
