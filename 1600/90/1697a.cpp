8bde700a08e89ff14fc5d77d3bb9824bd1fb5c77
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2022-06-15 18:37:17 +0300

    Add solution for problem 1697A

---
+++ 1600/90/1697a.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& a, unsigned m)
+{
+    unsigned d = 0;
+    for (const unsigned x : a) {
+        if (m < x) {
+            d += x - m;
+            m += x - m;
+        }
+        m -= x;
+    }
+
+    answer(d);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    unsigned m;
+    std::cin >> m;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a, m);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
