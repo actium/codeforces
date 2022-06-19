1854fe8235115ee7819281b801250db408de2ff9
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2022-06-19 10:53:22 +0300

    Add solution for problem 1692B

---
+++ 1600/90/1692b.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <set>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    const std::set<unsigned> s(a.cbegin(), a.cend());
+
+    answer(s.size() - (a.size() - s.size()) % 2);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
