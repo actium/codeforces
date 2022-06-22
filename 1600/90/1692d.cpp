f71b7362ccbdd5266093552b4aa019d42f325fe0
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2022-06-22 11:00:37 +0300

    Add solution for problem 1692D

---
+++ 1600/90/1692d.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <set>
+#include <string>
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+std::string time_to_string(unsigned t)
+{
+    const unsigned h = t / 60, m = t % 60;
+
+    char s[6];
+    snprintf(s, sizeof(s), "%02u:%02u", h, m);
+    return s;
+}
+
+void solve(const std::string& s, unsigned x)
+{
+    unsigned t = std::stoi(s.substr(0, 2)) * 60 + std::stoi(s.substr(3));
+
+    std::set<unsigned> v;
+    while (v.count(t) == 0) {
+        v.insert(t);
+        t = (t + x) % (24 * 60);
+    }
+
+    unsigned k = 0;
+    for (const unsigned t : v) {
+        const std::string s = time_to_string(t);
+        if (s == std::string(s.rbegin(), s.rend()))
+            ++k;
+    }
+
+    answer(k);
+}
+
+void test_case()
+{
+    std::string s;
+    std::cin >> s;
+
+    unsigned x;
+    std::cin >> x;
+
+    solve(s, x);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
