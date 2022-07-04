642b519400de9aacbc5e29b35c56890410984de8
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2022-07-04 10:59:49 +0300

    Add solution for problem 1698A

---
+++ 1600/90/1698a.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+
+#include <cstdlib>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    const size_t n = a.size();
+
+    answer(a[rand() % n]);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
