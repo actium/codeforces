e9cee81c1a76c3e6609ddb4900cd6fcdd9b5f894
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2022-07-21 08:53:16 +0300

    Add solution for problem 1703A

---
+++ 1700/00/1703a.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(std::string& s)
+{
+    for (char& c : s)
+        c = tolower(c);
+
+    answer(s == "yes");
+}
+
+void test_case()
+{
+    std::string s;
+    std::cin >> s;
+
+    solve(s);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
