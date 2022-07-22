c02a31753801bc7a7d0b2d736bfa4832c08fd9b8
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2022-07-22 06:25:30 +0300

    Add solution for problem 1703B

---
+++ 1700/00/1703b.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::string& s)
+{
+    unsigned b[26] = {};
+    for (const char c : s)
+        b[c-'A'] = 1;
+
+    unsigned k = s.length();
+    for (const unsigned x : b)
+        k += x;
+
+    answer(k);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::string s;
+    std::cin >> s;
+
+    solve(s);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
