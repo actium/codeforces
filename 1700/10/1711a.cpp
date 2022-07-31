0798640fadabd38fe7295dd65909cb625991557a
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2022-07-31 06:41:09 +0300

    Add solution for problem 1711A

---
+++ 1700/10/1711a.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+
+void answer(const std::vector<unsigned>& v)
+{
+    const char* separator = "";
+    for (const unsigned x : v) {
+        std::cout << separator << x;
+        separator = " ";
+    }
+    std::cout << '\n';
+}
+
+void solve(size_t n)
+{
+    std::vector<unsigned> p(n);
+    for (size_t i = 0; i < n; ++i)
+        p[i] = 1 + (i + 1) % n;
+
+    answer(p);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    solve(n);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
