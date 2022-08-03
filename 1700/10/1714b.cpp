27b4c2b6d346dcfa36f35adee7a5e4afc39304ec
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2022-08-03 07:11:16 +0300

    Add solution for problem 1714B

---
+++ 1700/10/1714b.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    const size_t n = a.size();
+
+    unsigned k = 0;
+
+    std::vector<unsigned> v(1 + n);
+    for (size_t i = 0; i < n; ++i) {
+        k = std::max(k, v[a[i]]);
+        v[a[i]] = 1 + i;
+    }
+
+    answer(k);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
