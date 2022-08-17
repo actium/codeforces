9ad5be71e9e8924d3e066a7b1f0e9827338d5259
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2022-08-17 09:18:27 +0300

    Add solution for problem 1712B

---
+++ 1700/10/1712b.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+
+void answer(const std::vector<unsigned>& v)
+{
+    const char* separator = "";
+    for (const unsigned x : v) {
+        std::cout << separator << x;
+        separator = " ";
+    }
+    std::cout << '\n';
+}
+
+void solve(unsigned n)
+{
+    std::vector<unsigned> p(n);
+    for (unsigned i = 1; i <= n; ++i)
+        p[i-1] = i;
+
+    for (size_t i = 1; i < n; i += 2)
+        std::swap(p[n-i], p[n-i-1]);
+
+    answer(p);
+}
+
+void test_case()
+{
+    unsigned n;
+    std::cin >> n;
+
+    solve(n);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
