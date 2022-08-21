2bdf8077950a64fdd019cedeb3b77b5ac161d1d8
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2022-08-21 10:42:04 +0300

    Add solution for problem 1719A

---
+++ 1700/10/1719a.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "Burenka", "Tonya" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(unsigned n, unsigned m)
+{
+    answer((n + m) % 2 == 0);
+}
+
+void test_case()
+{
+    unsigned n, m;
+    std::cin >> n >> m;
+
+    solve(n, m);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
