388105ee5e2269e33289d44870c5520efa74e2b2
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2022-08-30 15:50:16 +0300

    Add solution for problem 1692G

---
+++ 1600/90/1692g.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& a, size_t k)
+{
+    const size_t n = a.size();
+
+    unsigned q = 0;
+    for (size_t i = 0, j = 1; j < n; ++j) {
+        if (a[j-1] < 2 * a[j]) {
+            q += (j - i >= k);
+        } else {
+            i = j;
+        }
+    }
+
+    answer(q);
+}
+
+void test_case()
+{
+    size_t n, k;
+    std::cin >> n >> k;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a, k);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
