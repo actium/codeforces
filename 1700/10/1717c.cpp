0a4c9da9b397276227fc8f0fb2f5103ea495af14
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2022-09-08 12:48:27 +0300

    Add solution for problem 1717C

---
+++ 1700/10/1717c.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(const std::vector<unsigned>& a, const std::vector<unsigned>& b)
+{
+    const size_t n = a.size();
+
+    for (size_t i = 0; i < n; ++i) {
+        if (a[i] > b[i])
+            return answer(false);
+
+        if (a[i] < b[i] && b[i] > b[(i+1)%n] + 1)
+            return answer(false);
+    }
+
+    answer(true);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n), b(n);
+    std::cin >> a >> b;
+
+    solve(a, b);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
