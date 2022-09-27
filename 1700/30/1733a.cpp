5dd1c6b3ed9a8bd81c980791d464b28011f07bca
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2022-09-27 10:25:24 +0300

    Add solution for problem 1733A

---
+++ 1700/30/1733a.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+
+using integer = unsigned long long;
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(integer x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& a, size_t k)
+{
+    const size_t n = a.size();
+
+    std::vector<unsigned> s(k);
+    for (size_t i = 0; i < n; ++i)
+        s[i%k] = std::max(s[i%k], a[i]);
+
+    integer t = 0;
+    for (size_t i = 0; i < k; ++i)
+        t += s[i];
+
+    answer(t);
+}
+
+void test_case()
+{
+    size_t n, k;
+    std::cin >> n >> k;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a, k);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
