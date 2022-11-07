a0d60da159077d93d9bcd9dd23859862d2bb8564
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2022-11-07 16:39:42 +0300

    Add solution for problem 1747A

---
+++ 1700/40/1747a.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+
+using integer = long long;
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(integer x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<int>& a)
+{
+    integer s = 0;
+    for (const int x : a)
+        s += x;
+
+    answer(std::abs(s));
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<int> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
