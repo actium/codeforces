e6c580466d4c48b3362e8914019dfd41fa99b3b2
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2022-11-13 12:26:35 +0300

    Add solution for problem 1748A

---
+++ 1700/40/1748a.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(unsigned n)
+{
+    answer((n + 1) / 2);
+}
+
+void test_case()
+{
+    unsigned n;
+    std::cin >> n;
+
+    solve(n);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
