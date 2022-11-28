650529e7d106923ea75cfee4933dcb491dfc26d3
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2022-11-28 09:36:34 +0300

    Add solution for problem 1758A

---
+++ 1700/50/1758a.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+void answer(const std::string& x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::string& s)
+{
+    answer(s + std::string(s.rbegin(), s.rend()));
+}
+
+void test_case()
+{
+    std::string s;
+    std::cin >> s;
+
+    solve(s);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
