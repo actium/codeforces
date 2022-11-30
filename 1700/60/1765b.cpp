b2444fd2064d7332ea0405869a983413e07b7473
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2022-11-30 09:32:02 +0300

    Add solution for problem 1765B

---
+++ 1700/60/1765b.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(const std::string& s)
+{
+    const size_t n = s.length();
+
+    for (size_t i = 2; i < n; i += 3) {
+        if (s[i] != s[i-1])
+            return answer(false);
+    }
+
+    answer(n % 3 < 2);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::string s;
+    std::cin >> s;
+
+    solve(s);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
