2e5ebfea04d878f5c2d9c519ab0a68b73ac980e0
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2022-12-01 09:26:07 +0300

    Add solution for problem 1758B

---
+++ 1700/50/1758b.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+
+void answer(const std::vector<unsigned>& v)
+{
+    const char* separator = "";
+    for (const unsigned x : v) {
+        std::cout << separator << x;
+        separator = " ";
+    }
+    std::cout << '\n';
+}
+
+void solve(unsigned n)
+{
+    if (n % 2 == 1)
+        return answer(std::vector<unsigned>(n, 1));
+
+    std::vector<unsigned> a(n, 2);
+    a[0] = 1;
+    a[n-1] = 3;
+
+    answer(a);
+}
+
+void test_case()
+{
+    unsigned n;
+    std::cin >> n;
+
+    solve(n);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
