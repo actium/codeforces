f0ffe6ddaff6baad49d8c83b5af0b5caaccecff8
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2022-12-24 00:18:32 +0300

    Add solution for problem edu:8/1B

---
+++ edu/08/1b.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+
+template <typename T, typename U>
+std::istream& operator >>(std::istream& input, std::pair<T, U>& v)
+{
+    return input >> v.first >> v.second;
+}
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(const std::vector<unsigned>& v)
+{
+    const char* separator = "";
+    for (const unsigned x : v) {
+        std::cout << separator << x;
+        separator = " ";
+    }
+    std::cout << '\n';
+}
+
+void solve(size_t n, const std::vector<std::pair<unsigned, unsigned>>& e)
+{
+    std::vector<unsigned> d(n);
+    for (const auto& x : e) {
+        ++d[x.first-1];
+        ++d[x.second-1];
+    }
+
+    answer(d);
+}
+
+void test_case()
+{
+    size_t n, m;
+    std::cin >> n >> m;
+
+    std::vector<std::pair<unsigned, unsigned>> e(m);
+    std::cin >> e;
+
+    solve(n, e);
+}
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
