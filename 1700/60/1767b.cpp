3de850615807462343bac2c38c051774c1770925
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2022-12-29 00:13:39 +0300

    Add solution for problem 1767B

---
+++ 1700/60/1767b.cpp
@@ -0,0 +1,53 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(std::vector<unsigned>& a)
+{
+    unsigned x = a[0];
+
+    std::sort(a.begin(), a.end());
+
+    for (const unsigned h : a) {
+        if (h > x)
+            x = (x + h + 1) / 2;
+    }
+
+    answer(x);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
