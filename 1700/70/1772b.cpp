21d36c774ac0dce0bbe8cdd70bdb7aead81170f8
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2022-12-29 00:07:32 +0300

    Add solution for problem 1772B

---
+++ 1700/70/1772b.cpp
@@ -0,0 +1,44 @@
+#include <algorithm>
+#include <array>
+#include <iostream>
+
+template <typename T, size_t N>
+std::istream& operator >>(std::istream& input, std::array<T, N>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(const std::array<unsigned, 4>& a)
+{
+    const auto range = std::minmax_element(a.begin(), a.end());
+
+    answer(range.first - a.begin() + range.second - a.begin() == 3);
+}
+
+void test_case()
+{
+    std::array<unsigned, 4> a;
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
