1f8b9706cb9b02a4eb132c3746eb608a95d6ac1a
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2022-12-31 00:01:29 +0300

    Add solution for problem 1763A

---
+++ 1700/60/1763a.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    unsigned lb = ~0u, ub = 0;
+    for (const unsigned x : a) {
+        lb &= x;
+        ub |= x;
+    }
+
+    answer(ub - lb);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
