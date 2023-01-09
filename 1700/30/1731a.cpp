c66efe26499c12f8920d7ddc9ffed31085f70d03
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2023-01-10 00:00:52 +0300

    Add solution for problem 1731A

---
+++ 1700/30/1731a.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+
+using integer = unsigned long long;
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(integer x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    integer p = 1;
+    for (const unsigned x : a)
+        p *= x;
+
+    p += a.size() - 1;
+
+    answer(p * 2022);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
