8f9dac7ad8cd7d4be895f6674bcbc19ae407c5c9
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2023-01-18 00:26:04 +0300

    Add solution for problem 1783A

---
+++ 1700/80/1783a.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(const std::vector<unsigned>& v)
+{
+    std::cout << "YES" << '\n';
+
+    const char* separator = "";
+    for (const unsigned x : v) {
+        std::cout << separator << x;
+        separator = " ";
+    }
+    std::cout << '\n';
+}
+
+void no_answer()
+{
+    std::cout << "NO" << '\n';
+}
+
+void solve(std::vector<unsigned>& a)
+{
+    if (a.front() == a.back())
+        return no_answer();
+
+    std::swap(a[0], a[1]);
+    std::swap(a.front(), a.back());
+
+    answer(a);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
