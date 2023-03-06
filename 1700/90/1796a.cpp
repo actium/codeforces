60078e1fe45284dbaef22c666915d2b5fb8550a3
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2023-03-07 00:12:12 +0300

    Add solution for problem 1796A

---
+++ 1700/90/1796a.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(const std::string& s)
+{
+    answer(std::string("FBFFBFFBFBFFBFFBF").find(s) != std::string::npos);
+}
+
+void test_case()
+{
+    size_t k;
+    std::cin >> k;
+
+    std::string s;
+    std::cin >> s;
+
+    solve(s);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
