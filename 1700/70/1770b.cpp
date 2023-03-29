20af5d166e68628adef5662dc3a0157d10f5f0f5
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2023-03-29 07:23:16 +0300

    Add solution for problem 1770B

---
+++ 1700/70/1770b.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+
+void answer(const std::vector<unsigned>& v)
+{
+    const char* separator = "";
+    for (const unsigned x : v) {
+        std::cout << separator << x;
+        separator = " ";
+    }
+    std::cout << '\n';
+}
+
+void solve(unsigned n, unsigned k)
+{
+    std::vector<unsigned> p(n);
+    for (unsigned i = k - 1; i < n; i += k)
+        p[i] = (i + 1) / k;
+
+    for (unsigned i = 0, x = n; i < n; ++i) {
+        if (p[i] == 0)
+            p[i] = x--;
+    }
+
+    answer(p);
+}
+
+void test_case()
+{
+    unsigned n, k;
+    std::cin >> n >> k;
+
+    solve(n, k);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
