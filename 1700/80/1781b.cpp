ca9034195b16ed4ca74018e19fe34bd474fd6a6e
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2023-04-08 10:55:22 +0300

    Add solution for problem 1781B

---
+++ 1700/80/1781b.cpp
@@ -0,0 +1,54 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(std::vector<unsigned>& a)
+{
+    const size_t n = a.size();
+
+    std::sort(a.begin(), a.end());
+
+    unsigned k = 0;
+    for (size_t i = 0; i <= n; ++i) {
+        if ((i == 0 || a[i-1] < i) && (i == n || a[i] > i))
+            ++k;
+    }
+
+    answer(k);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
