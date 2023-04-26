bb839bd24cbba8aa2be69e1b2492f7e20972a7d2
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2023-04-26 11:06:28 +0300

    Add solution for problem 1780B

---
+++ 1700/80/1780b.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <numeric>
+#include <vector>
+
+using integer = unsigned long long;
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(integer x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    integer s = 0;
+    for (const unsigned q : a)
+        s += q;
+
+    integer x = 0, p = 0;
+    for (const unsigned q : a) {
+        p += q;
+        if (p < s)
+            x = std::max(x, std::gcd(p, s - p));
+    }
+
+    answer(x);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
