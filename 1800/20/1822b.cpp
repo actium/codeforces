6ccace8ec11bbb948c1df66848adf0935bf31024
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2023-05-02 14:20:06 +0300

    Add solution for problem 1822B

---
+++ 1800/20/1822b.cpp
@@ -0,0 +1,50 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using integer = long long;
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(integer x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(std::vector<int>& a)
+{
+    const size_t n = a.size();
+
+    std::sort(a.begin(), a.end());
+
+    answer(std::max(1ll * a[0] * a[1], 1ll * a[n-1] * a[n-2]));
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<int> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
