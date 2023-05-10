f8cd1e56bf3f774b04cbb41ae2fc4002fb35023e
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2023-05-10 07:43:09 +0300

    Add solution for problem 1800D

---
+++ 1800/00/1800d.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::string& s)
+{
+    const size_t n = s.length();
+
+    unsigned k = 1;
+    for (size_t i = 2; i < n; ++i)
+        k += (s[i] != s[i-2]);
+
+    answer(k);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::string s;
+    std::cin >> s;
+
+    solve(s);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
