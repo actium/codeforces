43d88e1a98cb939168ce362a3f0452bff8993ca1
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2023-05-12 06:25:13 +0300

    Add solution for problem 1790D

---
+++ 1700/90/1790d.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <map>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    std::map<unsigned, unsigned> f;
+    for (const unsigned x : a)
+        ++f[x];
+
+    unsigned k = 0;
+    while (!f.empty()) {
+        auto it = f.begin();
+        for (unsigned x = it->first; x == it->first; ++x)
+            ++it;
+
+        for (auto jt = f.begin(); jt != it; ) {
+            if (--jt->second == 0)
+                jt = f.erase(jt);
+            else
+                ++jt;
+        }
+
+        ++k;
+    }
+
+    answer(k);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
