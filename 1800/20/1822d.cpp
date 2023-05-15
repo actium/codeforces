8a8184f8590e3c2193f9c5b24d4db47607518194
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2023-05-15 09:42:53 +0300

    Add solution for problem 1822D

---
+++ 1800/20/1822d.cpp
@@ -0,0 +1,62 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(const std::vector<unsigned>& v)
+{
+    const char* separator = "";
+    for (const unsigned x : v) {
+        std::cout << separator << x;
+        separator = " ";
+    }
+    std::cout << '\n';
+}
+
+void no_answer()
+{
+    std::cout << -1 << '\n';
+}
+
+void solve(unsigned n)
+{
+    if (n == 1)
+        return answer({1});
+
+    if (n % 2 == 1)
+        return no_answer();
+
+    std::vector<unsigned> a(n);
+    for (unsigned i = 0; i < n; i += 2) {
+        a[i] = n - i;
+        a[i+1] = 1 + i;
+    }
+    answer(a);
+}
+
+void test_case()
+{
+    unsigned n;
+    std::cin >> n;
+
+    solve(n);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
