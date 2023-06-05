59cfafb3cf01cda85739fcb7be1e1f776fa59f4d
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2023-06-05 10:11:39 +0300

    Add solution for problem 1832C

---
+++ 1800/30/1832c.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    const size_t n = a.size();
+
+    unsigned k = 1, d = 1;
+    for (size_t i = 1; i < n; ++i) {
+        if (a[i] > a[i-1]) {
+            k += (d != 2);
+            d = 2;
+        }
+        if (a[i] < a[i-1]) {
+            k += (d != 0);
+            d = 0;
+        }
+    }
+
+    answer(k);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
