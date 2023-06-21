847357ab24dae76146153e9c80be88f9b3370452
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2023-06-21 08:45:16 +0300

    Add solution for problem 1836A

---
+++ 1800/30/1836a.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    unsigned f[100] = {};
+    for (const unsigned x : a)
+        ++f[x];
+
+    for (size_t i = 1; i < 100; ++i) {
+        if (f[i] > f[i-1])
+            return answer(false);
+    }
+    answer(true);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
