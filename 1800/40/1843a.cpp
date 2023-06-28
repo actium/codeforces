ee2000b984491fb2845fa09709e945390b481e2b
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2023-06-28 09:54:57 +0300

    Add solution for problem 1843A

---
+++ 1800/40/1843a.cpp
@@ -0,0 +1,50 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(std::vector<unsigned>& a)
+{
+    std::sort(a.begin(), a.end());
+
+    unsigned s = 0;
+    for (size_t i = 0, j = a.size() - 1; i < j; ++i, --j)
+        s += a[j] - a[i];
+
+    answer(s);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
