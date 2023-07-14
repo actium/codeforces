2c5525e170de5dacd2a7e1c404b24e625cef7ae8
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2023-07-14 09:38:39 +0300

    Add solution for problem 1815A

---
+++ 1800/10/1815a.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(const std::vector<int>& a)
+{
+    const size_t n = a.size();
+    if (n % 2 == 1)
+        return answer(true);
+
+    long long b = 0;
+    for (size_t i = 1; i < n; i += 2)
+        b += a[i] - a[i-1];
+
+    answer(b >= 0);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<int> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
