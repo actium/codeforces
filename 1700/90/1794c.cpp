243fc35bb36fdaa67d072bcc9eaab85ec00cab58
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2023-08-07 08:12:14 +0300

    Add solution for problem 1794C

---
+++ 1700/90/1794c.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(const std::vector<size_t>& v)
+{
+    const char* separator = "";
+    for (const unsigned x : v) {
+        std::cout << separator << x;
+        separator = " ";
+    }
+    std::cout << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    const size_t n = a.size();
+
+    std::vector<size_t> c(n);
+    for (size_t i = 0, j = 0; j < n; ++j) {
+        while (i < j && a[i] < j - i + 1)
+            ++i;
+
+        c[j] = j - i + 1;
+    }
+
+    answer(c);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
