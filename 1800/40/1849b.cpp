9a8b08ccce10283c9677ba1e1f683d347c280e50
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2023-08-20 11:40:14 +0300

    Add solution for problem 1849B

---
+++ 1800/40/1849b.cpp
@@ -0,0 +1,69 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(const std::vector<size_t>& v)
+{
+    const char* separator = "";
+    for (const size_t x : v) {
+        std::cout << separator << 1 + x;
+        separator = " ";
+    }
+    std::cout << '\n';
+}
+
+void solve(std::vector<unsigned>& a, unsigned k)
+{
+    const size_t n = a.size();
+
+    for (size_t i = 0; i < n; ++i) {
+        a[i] %= k;
+
+        if (a[i] == 0)
+            a[i] = k;
+    }
+
+    std::vector<size_t> p(n);
+    for (size_t i = 0; i < n; ++i)
+        p[i] = i;
+
+    std::sort(p.begin(), p.end(), [&a](size_t i, size_t j) {
+        return a[i] != a[j] ? a[i] > a[j] : i < j;
+    });
+
+    answer(p);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    unsigned k;
+    std::cin >> k;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a, k);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
