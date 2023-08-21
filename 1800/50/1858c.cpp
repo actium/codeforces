3327169fe42d37d4dcfaa9b9633a2c485c3484f7
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2023-08-21 10:41:00 +0300

    Add solution for problem 1858C

---
+++ 1800/50/1858c.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(const std::vector<unsigned>& v)
+{
+    const char* separator = "";
+    for (const unsigned x : v) {
+        std::cout << separator << x;
+        separator = " ";
+    }
+    std::cout << '\n';
+}
+
+void solve(unsigned n)
+{
+    std::vector<unsigned> a;
+    for (unsigned i = 1; i <= n; i += 2) {
+        for (unsigned j = i; j <= n; j *= 2)
+            a.push_back(j);
+    }
+
+    answer(a);
+}
+
+void test_case()
+{
+    unsigned n;
+    std::cin >> n;
+
+    solve(n);
+}
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
