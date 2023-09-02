3fed2e7fccd60dc0840851ba5a5f27db83803802
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2023-09-02 13:59:26 +0300

    Add solution for problem 1776H

---
+++ 1700/70/1776h.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& a, const std::vector<unsigned>& b)
+{
+    const size_t n = a.size();
+
+    unsigned k = 0;
+    for (size_t i = n, j = n-1; i-- > 0; ) {
+        if (a[i] == b[j])
+            --j;
+        else
+            ++k;
+    }
+
+    answer(k);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n), b(n);
+    std::cin >> a >> b;
+
+    solve(a, b);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
