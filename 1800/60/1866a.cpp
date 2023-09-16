126078f4d797aca7458c964bd04de94094057156
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2023-09-16 16:06:02 +0300

    Add solution for problem 1866A

---
+++ 1800/60/1866a.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<int>& a)
+{
+    unsigned k = ~0u;
+    for (const int x : a)
+        k = std::min<unsigned>(k, abs(x));
+
+    answer(k);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t n;
+    std::cin >> n;
+
+    std::vector<int> a(n);
+    std::cin >> a;
+
+    solve(a);
+
+    return 0;
+}
