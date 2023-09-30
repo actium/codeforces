bd70e587ce9c9d8ae2d21d7c7462a566cc180b17
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2023-09-30 04:09:27 +0300

    Add solution for problem 1863B

---
+++ 1800/60/1863b.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& p)
+{
+    const size_t n = p.size();
+
+    std::vector<size_t> x(n);
+    for (size_t i = 0; i < n; ++i)
+        x[p[i]-1] = i;
+
+    unsigned k = 0;
+    for (size_t i = 1; i < n; ++i)
+        k += x[i-1] > x[i];
+
+    answer(k);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> p(n);
+    std::cin >> p;
+
+    solve(p);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
