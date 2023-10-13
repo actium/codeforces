35f168a61f7eacbdac5eb4c40473e4420c670e5b
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2023-10-13 06:40:13 +0300

    Add solution for problem 1847C

---
+++ 1800/40/1847c.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    bool p[256] = { true };
+
+    unsigned x = 0, v = 0;
+    for (const unsigned q : a) {
+        x ^= q;
+        for (unsigned i = 0; i < 256; ++i) {
+            if (p[i])
+                v = std::max(v, i ^ x);
+        }
+        p[x] = true;
+    }
+
+    answer(v);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
