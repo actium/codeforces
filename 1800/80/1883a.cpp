be739ba8bc51bc46cf0c326c2108b6d542eeacac
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2023-11-11 07:10:02 +0300

    Add solution for problem 1883A

---
+++ 1800/80/1883a.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::string& s)
+{
+    unsigned k = 0, x = 1;
+    for (const char c : s) {
+        const unsigned d = (c == '0' ? 10 : c - '0');
+        k += (d <= x ? x - d : d - x) + 1;
+        x = d;
+    }
+
+    answer(k);
+}
+
+void test_case()
+{
+    std::string s;
+    std::cin >> s;
+
+    solve(s);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
