4a28d13b403fe8dbbaa367bd565f33d97f160d53
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2023-11-15 08:04:08 +0300

    Add solution for problem 1877A

---
+++ 1800/70/1877a.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(int x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<int>& a)
+{
+    int s = 0;
+    for (const int x : a)
+        s += x;
+
+    answer(-s);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<int> a(n-1);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
