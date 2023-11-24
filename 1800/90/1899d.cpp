077c94474ffb135bdc960e4b5261284bb3b79529
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2023-11-24 06:08:54 +0300

    Add solution for problem 1899D

---
+++ 1800/90/1899d.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <map>
+#include <vector>
+
+using integer = unsigned long long;
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(integer x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    integer k = 0;
+
+    std::map<unsigned, unsigned> f;
+    for (const unsigned x : a)
+        k += f[x + (x == 1)]++;
+
+    answer(k);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
