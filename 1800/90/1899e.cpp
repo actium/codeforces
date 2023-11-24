16805a9fdb76bf09bb5f07f1accabb9bbb1b2b75
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2023-11-24 06:13:11 +0300

    Add solution for problem 1899E

---
+++ 1800/90/1899e.cpp
@@ -0,0 +1,59 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void no_answer()
+{
+    std::cout << -1 << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    const size_t n = a.size();
+
+    const auto it = std::min_element(a.begin(), a.end());
+    for (size_t i = it - a.begin() + 1; i < n; ++i) {
+        if (a[i] < a[i-1])
+            return no_answer();
+    }
+
+    answer(it - a.begin());
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
