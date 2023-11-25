23570234126cdb462dd1e92cf9e5cef9ac98d25b
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2023-11-25 21:19:51 +0300

    Add solution for problem 1896B

---
+++ 1800/90/1896b.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::string& s)
+{
+    const size_t a = s.find('A'), b = s.rfind('B');
+
+    answer(a == std::string::npos || b == std::string::npos || a > b ? 0 : b - a);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::string s;
+    std::cin >> s;
+
+    solve(s);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
