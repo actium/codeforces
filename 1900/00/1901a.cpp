650759d50479aba58a12c8d4c25e6eae6b9a7f65
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2023-11-26 07:39:02 +0300

    Add solution for problem 1901A

---
+++ 1900/00/1901a.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& a, unsigned x)
+{
+    const size_t n = a.size();
+
+    unsigned p = 0, v = 0;
+    for (const unsigned q : a) {
+        v = std::max(v, q - p);
+        p = q;
+    }
+
+    answer(std::max(v, 2 * (x - p)));
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    unsigned x;
+    std::cin >> x;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a, x);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
