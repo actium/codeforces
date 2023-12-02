926c66f2696933e255e7368635aa64d4d3fb1703
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2023-12-02 06:25:30 +0300

    Add solution for problem 1903A

---
+++ 1900/00/1903a.cpp
@@ -0,0 +1,47 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(const std::vector<unsigned>& a, size_t k)
+{
+    answer(k > 1 || std::is_sorted(a.begin(), a.end()));
+}
+
+void test_case()
+{
+    size_t n, k;
+    std::cin >> n >> k;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a, k);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
