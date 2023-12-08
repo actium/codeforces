064c8d662e254d94b8b1bdb2822bbfd8ca02c750
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2023-12-08 05:40:45 +0300

    Add solution for problem 1907C

---
+++ 1900/00/1907c.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::string& s)
+{
+    const size_t n = s.length();
+
+    unsigned f[26] = {}, x = 0;
+    for (const char c : s)
+        x = std::max(x, ++f[c-'a']);
+
+    answer(x <= n / 2 ? n % 2 : 2 * x - n);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::string s;
+    std::cin >> s;
+
+    solve(s);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
