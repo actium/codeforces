6c940e7927bdb49b7c9ea2d6e1461ccff9b5aba0
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2023-12-25 10:30:39 +0300

    Add solution for problem edu:5/3A

---
+++ edu/05/3a.cpp
@@ -0,0 +1,190 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+struct Query {
+    unsigned l;
+    unsigned r;
+    int v;
+};
+
+std::istream& operator >>(std::istream& input, Query& q)
+{
+    return input >> q.l >> q.r >> q.v;
+}
+
+class SegmentTree {
+    struct Node {
+        static constexpr int oo = 2000000000;
+
+        long long range_size = 1;
+        long long sum = 0;
+        long long max_prefix = 0;
+        long long max_suffix = 0;
+        long long max_sum = 0;
+
+        int common_value = oo;
+
+        void initialize(int value)
+        {
+            sum = value;
+            max_prefix = std::max(0, value);
+            max_suffix = std::max(0, value);
+            max_sum = std::max(0, value);
+        }
+
+        void update(int value)
+        {
+            sum = range_size * value;
+            max_prefix = std::max(sum, 0ll);
+            max_suffix = std::max(sum, 0ll);
+            max_sum = std::max(sum, 0ll);
+
+            common_value = value;
+        }
+
+        void propagate_updates(Node& lhs, Node& rhs)
+        {
+            if (common_value != oo) {
+                lhs.update(common_value);
+                rhs.update(common_value);
+                common_value = oo;
+            }
+        }
+
+        static Node compose(const Node& lhs, const Node& rhs)
+        {
+            return {
+                .range_size = lhs.range_size + rhs.range_size,
+                .sum = lhs.sum + rhs.sum,
+                .max_prefix = std::max(lhs.max_prefix, lhs.sum + rhs.max_prefix),
+                .max_suffix = std::max(lhs.max_suffix + rhs.sum, rhs.max_suffix),
+                .max_sum = std::max(lhs.max_suffix + rhs.max_prefix, std::max(lhs.max_sum, rhs.max_sum)),
+            };
+        }
+    };
+
+public:
+    explicit SegmentTree(unsigned size)
+        : size_(1 << __builtin_clz(1) - __builtin_clz(size) + 1)
+        , nodes_(2 * size_)
+    {
+        for (unsigned i = size_ - 1; i > 0; --i)
+            nodes_[i] = Node::compose(nodes_[i<<1|0], nodes_[i<<1|1]);
+    }
+
+    void set(unsigned index, unsigned value)
+    {
+        set({ 1, 0, size_ }, index, value);
+    }
+
+    template <typename... T>
+    void update(unsigned range_begin, unsigned range_end, T&&... arguments)
+    {
+        update({ 1, 0, size_ }, range_begin, range_end, std::forward<T>(arguments)...);
+    }
+
+    Node query(unsigned range_begin, unsigned range_end)
+    {
+        return query({ 1, 0, size_ }, range_begin, range_end);
+    }
+
+private:
+    struct Subtree {
+        unsigned root_index;
+        unsigned range_begin;
+        unsigned range_end;
+    };
+
+    void set(const Subtree& subtree, unsigned index, unsigned value)
+    {
+        if (subtree.range_begin + 1 == subtree.range_end) {
+            nodes_[subtree.root_index].initialize(value);
+            return;
+        }
+
+        nodes_[subtree.root_index].propagate_updates(nodes_[subtree.root_index<<1|0], nodes_[subtree.root_index<<1|1]);
+
+        const unsigned mid = (subtree.range_begin + subtree.range_end) / 2;
+        if (index < mid) {
+            set({ subtree.root_index<<1|0, subtree.range_begin, mid }, index, value);
+        } else {
+            set({ subtree.root_index<<1|1, mid, subtree.range_end }, index, value);
+        }
+        nodes_[subtree.root_index] = Node::compose(nodes_[subtree.root_index<<1|0], nodes_[subtree.root_index<<1|1]);
+    }
+
+    template <typename... T>
+    void update(const Subtree& subtree, unsigned range_begin, unsigned range_end, T&&... arguments)
+    {
+        if (range_end <= subtree.range_begin || subtree.range_end <= range_begin)
+            return;
+
+        if (range_begin <= subtree.range_begin && subtree.range_end <= range_end) {
+            nodes_[subtree.root_index].update(std::forward<T>(arguments)...);
+            return;
+        }
+
+        nodes_[subtree.root_index].propagate_updates(nodes_[subtree.root_index<<1|0], nodes_[subtree.root_index<<1|1]);
+
+        const unsigned mid = (subtree.range_begin + subtree.range_end) / 2;
+        update({ subtree.root_index<<1|0, subtree.range_begin, mid }, range_begin, range_end, std::forward<T>(arguments)...);
+        update({ subtree.root_index<<1|1, mid, subtree.range_end }, range_begin, range_end, std::forward<T>(arguments)...);
+        nodes_[subtree.root_index] = Node::compose(nodes_[subtree.root_index<<1|0], nodes_[subtree.root_index<<1|1]);
+    }
+
+    Node query(const Subtree& subtree, unsigned range_begin, unsigned range_end)
+    {
+        if (range_end <= subtree.range_begin || subtree.range_end <= range_begin)
+            return {};
+
+        if (range_begin <= subtree.range_begin && subtree.range_end <= range_end)
+            return nodes_[subtree.root_index];
+
+        nodes_[subtree.root_index].propagate_updates(nodes_[subtree.root_index<<1|0], nodes_[subtree.root_index<<1|1]);
+
+        const unsigned mid = (subtree.range_begin + subtree.range_end) / 2;
+        return Node::compose(
+            query({ subtree.root_index<<1|0, subtree.range_begin, mid }, range_begin, range_end),
+            query({ subtree.root_index<<1|1, mid, subtree.range_end }, range_begin, range_end)
+        );
+    }
+
+private:
+    const unsigned size_;
+
+    std::vector<Node> nodes_;
+
+}; // class SegmentTree
+
+void solve(unsigned n, const std::vector<Query>& qs)
+{
+    SegmentTree st(n);
+    for (const Query& q : qs) {
+        st.update(q.l, q.r, q.v);
+        std::cout << st.query(0, n).max_sum << '\n';
+    }
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    unsigned n, m;
+    std::cin >> n >> m;
+
+    std::vector<Query> qs(m);
+    std::cin >> qs;
+
+    solve(n, qs);
+
+    return 0;
+}
