1d2641eb308eb48712b6f84fa3d85732c0d84f41
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2023-12-29 08:17:47 +0300

    Add solution for problem 1915C

---
+++ 1900/10/1915c.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+
+#include <cmath>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    using integer = unsigned long long;
+
+    integer s = 0;
+    for (const unsigned x : a)
+        s += x;
+
+    integer r = sqrt(s);
+    if (r * r < s)
+        ++r;
+
+    answer(r * r == s);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
