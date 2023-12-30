c5b1cf68d94d0b061e7dd16cdf79fd1cba5c345f
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2023-12-30 06:48:38 +0300

    Add solution for problem 1800E2

---
+++ 1800/00/1800e2.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(const std::string& s, const std::string& t, size_t k)
+{
+    const size_t n = s.length();
+
+    unsigned b[26] = {};
+    for (size_t i = 0; i < n; ++i) {
+        ++b[s[i] - 'a'];
+        --b[t[i] - 'a'];
+    }
+
+    for (size_t i = 0; i < 26; ++i) {
+        if (b[i] != 0)
+            return answer(false);
+    }
+
+    if (n <= k)
+        return answer(s == t);
+
+    for (size_t i = n - k; i < k; ++i) {
+        if (s[i] != t[i])
+            return answer(false);
+    }
+
+    answer(true);
+}
+
+void test_case()
+{
+    size_t n, k;
+    std::cin >> n >> k;
+
+    std::string s, t;
+    std::cin >> s >> t;
+
+    solve(s, t, k);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
