df451f0d77724da89ddfcd800c60f0af6a7e8603
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2024-01-01 09:56:54 +0300

    Add solution for problem 1916C

---
+++ 1900/10/1916c.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+
+using integer = unsigned long long;
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(const std::vector<integer>& v)
+{
+    const char* separator = "";
+    for (const integer x : v) {
+        std::cout << separator << x;
+        separator = " ";
+    }
+    std::cout << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    const size_t n = a.size();
+
+    std::vector<integer> v(a.begin(), a.end());
+    for (size_t i = 1; i < n; ++i)
+        v[i] += v[i-1];
+
+    unsigned c = a[0] % 2;
+    for (size_t i = 1; i < n; ++i) {
+        c += a[i] % 2;
+        v[i] -= c / 3;
+        v[i] -= v[i] % 2;
+    }
+
+    answer(v);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
