1e40f5a5fc4502fd117438e983fd21032d2da2e3
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2024-02-02 07:22:33 +0300

    Add solution for problem 1101C

---
+++ 1100/00/1101c.cpp
@@ -0,0 +1,81 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T, typename U>
+std::istream& operator >>(std::istream& input, std::pair<T, U>& v)
+{
+    return input >> v.first >> v.second;
+}
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(const std::vector<unsigned>& v)
+{
+    const char* separator = "";
+    for (const unsigned x : v) {
+        std::cout << separator << x;
+        separator = " ";
+    }
+    std::cout << '\n';
+}
+
+void no_answer()
+{
+    std::cout << -1 << '\n';
+}
+
+void solve(const std::vector<std::pair<unsigned, unsigned>>& s)
+{
+    const size_t n = s.size();
+
+    std::vector<size_t> p(n);
+    for (size_t i = 0; i < n; ++i)
+        p[i] = i;
+
+    std::sort(p.begin(), p.end(), [&](size_t i, size_t j) { return s[i] < s[j]; });
+
+    std::vector<unsigned> c(n, 1);
+    {
+        unsigned r = s[p[0]].second;
+        for (const size_t i : p) {
+            if (s[i].first > r)
+                ++c[i];
+            else
+                r = std::max(r, s[i].second);
+        }
+    }
+
+    c[p.back()] == 2 ? answer(c) : no_answer();
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<std::pair<unsigned, unsigned>> s(n);
+    std::cin >> s;
+
+    solve(s);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
