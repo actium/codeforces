3ca1964ea7007edef4777f0cdbef51ba869c6a67
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2024-02-05 05:20:49 +0300

    Add solution for problem 587A

---
+++ 0500/80/587a.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& w)
+{
+    unsigned f[1000021] = {};
+    for (const unsigned x : w)
+        ++f[x];
+
+    unsigned k = 0;
+    for (size_t i = 0; i < 1000021; ++i) {
+        k += f[i] % 2;
+        f[i+1] += f[i] / 2;
+    }
+
+    answer(k);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> w(n);
+    std::cin >> w;
+
+    solve(w);
+
+    return 0;
+}
