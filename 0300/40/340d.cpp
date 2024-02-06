53b23628256414f7cebe718e227508600441b280
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2024-02-06 05:11:20 +0300

    Add solution for problem 340D

---
+++ 0300/40/340d.cpp
@@ -0,0 +1,46 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    std::vector<unsigned> s;
+    for (const unsigned x : a) {
+        const auto it = std::lower_bound(s.begin(), s.end(), x);
+        if (it == s.end())
+            s.push_back(x);
+        else
+            *it = x;
+    }
+
+    answer(s.size());
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+
+    return 0;
+}
