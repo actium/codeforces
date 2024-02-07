70e6467516ec489e3549cadb19c6a279865b0c18
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2024-02-07 05:53:23 +0300

    Add solution for problem 1927B

---
+++ 1900/20/1927b.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(const std::string& x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    unsigned h[26] = {};
+
+    std::string s;
+    for (const unsigned x : a) {
+        unsigned d = 0;
+        while (h[d] != x)
+            ++d;
+
+        s.push_back('a' + d);
+        ++h[d];
+    }
+
+    answer(s);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
