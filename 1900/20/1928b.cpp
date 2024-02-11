d80daf82eaba10b26888babe8e4659bccb0b2c3a
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2024-02-11 16:58:52 +0300

    Add solution for problem 1928B

---
+++ 1900/20/1928b.cpp
@@ -0,0 +1,59 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(std::vector<unsigned>& a)
+{
+    const size_t n = a.size();
+
+    std::sort(a.begin(), a.end());
+    a.erase(std::unique(a.begin(), a.end()), a.end());
+
+    unsigned k = 0;
+    for (auto it = a.begin(), jt = it; it != a.end(); ++it) {
+        while (jt != a.end() && *it + n > *jt)
+            ++jt;
+
+        k = std::max<unsigned>(k, std::distance(it, jt));
+    }
+
+    answer(k);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
