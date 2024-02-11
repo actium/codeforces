10b9fc23f90bfad9c9dad553eac03dc638790e10
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2024-02-11 16:54:59 +0300

    Add solution for problem 1928A

---
+++ 1900/20/1928a.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(unsigned a, unsigned b)
+{
+    answer(a % 2 == 0 && a / 2 != b || b % 2 == 0 && b / 2 != a);
+}
+
+void test_case()
+{
+    unsigned a, b;
+    std::cin >> a >> b;
+
+    solve(a, b);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
