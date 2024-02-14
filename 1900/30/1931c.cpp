d61c23259409b4e1a5b824eb02d9b70bfa357abc
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2024-02-14 06:59:48 +0300

    Add solution for problem 1931C

---
+++ 1900/30/1931c.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    const size_t n = a.size();
+
+    unsigned k = 1;
+    for (size_t i = 1; i < n && a[i] == a[i-1]; ++i)
+        ++k;
+
+    if (k == n)
+        return answer(0);
+
+    unsigned q = 1;
+    for (size_t i = n - 2; a[i] == a[i+1]; --i)
+        ++q;
+
+    answer(n - (a[0] == a[n-1] ? k + q : std::max(k, q)));
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
