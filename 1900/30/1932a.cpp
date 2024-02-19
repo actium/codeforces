811af169ade0e01f494b300b63792b15cc7033c8
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2024-02-19 06:40:23 +0300

    Add solution for problem 1932A

---
+++ 1900/30/1932a.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::string& s)
+{
+    unsigned k = 0;
+    for (size_t i = 0; s[i] != '*' || s[i+1] != '*'; ++i) {
+        if (s[i] == '@')
+            ++k;
+    }
+
+    answer(k);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::string s;
+    std::cin >> s;
+
+    solve(s + "**");
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
