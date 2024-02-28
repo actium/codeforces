575ddc0e6df8bd2ae3b7521ca77f9e6e67923a07
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2024-02-28 06:14:51 +0300

    Add solution for problem 1933A

---
+++ 1900/30/1933a.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<int>& a)
+{
+    unsigned s = 0;
+    for (const int x : a)
+        s += std::abs(x);
+
+    answer(s);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<int> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
