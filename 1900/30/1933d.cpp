58733437bf7ac4254274132d700d635ea3b52287
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2024-02-28 06:24:16 +0300

    Add solution for problem 1933D

---
+++ 1900/30/1933d.cpp
@@ -0,0 +1,52 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    const unsigned x = *std::min_element(a.begin(), a.end());
+
+    if (std::count(a.begin(), a.end(), x) == 1)
+        return answer(true);
+
+    answer(std::any_of(a.begin(), a.end(), [x](unsigned q) { return q % x != 0; }));
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
