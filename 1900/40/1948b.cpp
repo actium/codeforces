51dfd25992fa1b0b5873ed999f50ab08229f67b8
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2024-03-16 06:55:14 +0300

    Add solution for problem 1948B

---
+++ 1900/40/1948b.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(std::vector<unsigned>& a)
+{
+    while (a.size() > 1) {
+        const unsigned x = a.back();
+        a.pop_back();
+
+        if (x >= a.back())
+            continue;
+
+        const unsigned y = a.back();
+        a.pop_back();
+
+        if (y >= 10)
+            a.push_back(y / 10);
+
+        a.push_back(y % 10);
+
+        if (x < a.back())
+            return answer(false);
+    }
+
+    answer(true);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
