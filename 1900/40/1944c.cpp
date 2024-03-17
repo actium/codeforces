fd05632f78989f26679bd11fb36cf20c8805cd5f
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2024-03-17 05:44:23 +0300

    Add solution for problem 1944C

---
+++ 1900/40/1944c.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    const size_t n = a.size();
+
+    std::vector<unsigned> f(n);
+    for (const unsigned x : a)
+        ++f[x];
+
+    unsigned x = 0;
+    for (unsigned k = 0; x < n && f[x] != 0; ++x) {
+        if (f[x] == 1 && ++k == 2)
+            break;
+    }
+
+    answer(x);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
