2c9a6f4cf2be89624747a8868d4ca5b169df4eb2
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2024-03-18 05:23:54 +0300

    Add solution for problem 348A

---
+++ 0300/40/348a.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+
+using integer = unsigned long long;
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(integer x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    const size_t n = a.size();
+
+    integer t = 0;
+    for (const unsigned x : a)
+        t += x;
+
+    integer k = (t - 1) / (n - 1) + 1;
+    for (const unsigned x : a)
+        k = std::max<integer>(k, x);
+
+    answer(k);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+
+    return 0;
+}
