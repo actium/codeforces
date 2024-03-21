3c94289ae1803bcc35ad7d9a2d6f2aad633ab5d7
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2024-03-21 07:03:21 +0300

    Add solution for problem 1945A

---
+++ 1900/40/1945a.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void no_answer()
+{
+    std::cout << -1 << '\n';
+}
+
+void solve(unsigned a, unsigned b, unsigned c)
+{
+    const unsigned d = (3 - b % 3) % 3;
+    if (d <= c)
+        return answer(a + (b + c + 2) / 3);
+
+    no_answer();
+}
+
+void test_case()
+{
+    unsigned a, b, c;
+    std::cin >> a >> b >> c;
+
+    solve(a, b, c);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
