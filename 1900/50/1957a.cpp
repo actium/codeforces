a3dee04c264dad6cc1b1bcde0147afecdecff3ba
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2024-04-22 07:01:30 +0300

    Add solution for problem 1957A

---
+++ 1900/50/1957a.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    unsigned f[100] = {};
+    for (const unsigned x : a)
+        ++f[x-1];
+
+    unsigned k = 0;
+    for (unsigned i = 0; i < 100; ++i)
+        k += f[i] / 3;
+
+    answer(k);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
