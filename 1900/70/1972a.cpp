9da5b535724816c1dd884f477bcc33923d2f32b3
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2024-05-01 06:46:59 +0300

    Add solution for problem 1972A

---
+++ 1900/70/1972a.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& a, const std::vector<unsigned>& b)
+{
+    const size_t n = a.size();
+
+    unsigned k = 0;
+    for (size_t i = 0; i < n; ++i)
+        k += (a[i-k] > b[i]);
+
+    answer(k);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n), b(n);
+    std::cin >> a >> b;
+
+    solve(a, b);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
