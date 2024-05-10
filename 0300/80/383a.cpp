8dd9ecff3154170909a77d395963fe1d9d9d0c87
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2024-05-10 14:53:05 +0300

    Add solution for problem 383A

---
+++ 0300/80/383a.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+
+using integer = unsigned long long;
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(integer x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    integer k = 0, p = 0;
+    for (const unsigned x : a) {
+        if (x == 1)
+            ++p;
+        else
+            k += p;
+    }
+
+    answer(k);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+
+    return 0;
+}
