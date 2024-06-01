9c41ddd9dfb4f9f19b15940527d587af09980020
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2024-06-01 08:02:43 +0300

    Add solution for problem 825C

---
+++ 0800/20/825c.cpp
@@ -0,0 +1,51 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(std::vector<unsigned>& a, unsigned k)
+{
+    std::sort(a.begin(), a.end());
+
+    unsigned q = 0;
+    for (const unsigned x : a) {
+        while (2 * k < x) {
+            k *= 2;
+            ++q;
+        }
+        k = std::max(k, x);
+    }
+
+    answer(q);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t n;
+    std::cin >> n;
+
+    unsigned k;
+    std::cin >> k;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a, k);
+
+    return 0;
+}
