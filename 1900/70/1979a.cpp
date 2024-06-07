7eddc86030defeace920f49806300c2adfa6b785
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2024-06-07 10:25:07 +0300

    Add solution for problem 1979A

---
+++ 1900/70/1979a.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    const size_t n = a.size();
+
+    unsigned x = ~0u;
+    for (size_t i = 1; i < n; ++i)
+        x = std::min(x, std::max(a[i-1], a[i]));
+
+    answer(x - 1);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
