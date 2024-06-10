5b4caaeaf1989c5f267a1536d5eef3a0a43f1485
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2024-06-10 07:16:00 +0300

    Add solution for problem 1984C1

---
+++ 1900/80/1984c1.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+
+using integer = long long;
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(integer x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<int>& a)
+{
+    integer u = 0, v = 0;
+    for (const int x : a) {
+        u += x;
+        v = std::max<integer>(v + x, std::abs(u));
+    }
+
+    answer(v);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<int> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
