9ead95d803e7597b7fe1db7b2f96d19f9d11e4ec
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2024-06-12 09:01:39 +0300

    Add solution for problem 1985C

---
+++ 1900/80/1985c.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    unsigned k = 0;
+    {
+        unsigned long long s = 0, t = 0;
+        for (const unsigned x : a) {
+            s += x;
+            t = std::max<unsigned>(t, x);
+            k += (s == 2 * t);
+        }
+    }
+    answer(k);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
