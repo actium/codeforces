8a8f08cb5c1b7f38f7e2b51120b2a188d50a1c1e
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2024-06-28 07:51:29 +0300

    Add solution for problem 1989A

---
+++ 1900/80/1989a.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(int x, int y)
+{
+    answer(y >= -1);
+}
+
+void test_case()
+{
+    int x, y;
+    std::cin >> x >> y;
+
+    solve(x, y);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
