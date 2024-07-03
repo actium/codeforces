571b86d89958a46adea513852616c431d47b3b07
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2024-07-03 07:53:03 +0300

    Add solution for problem 1987B

---
+++ 1900/80/1987b.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+
+using integer = unsigned long long;
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(integer x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    const size_t n = a.size();
+
+    integer s = 0, t = 0;
+    for (size_t i = 0, x = 0; i < n; ++i) {
+        if (a[i] >= a[x])
+            x = i;
+
+        const integer d = a[x] - a[i];
+        s += d;
+        t = std::max(t, d);
+    }
+
+    answer(s + t);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
