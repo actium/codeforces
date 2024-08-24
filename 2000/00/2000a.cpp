e810fa98a98eeaaecc6832d23fad3588f11bcffa
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2024-08-24 06:03:30 +0300

    Add solution for problem 2000A

---
+++ 2000/00/2000a.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(unsigned a)
+{
+    answer(a > 101 && a < 110 || a > 1009 && a < 1100);
+}
+
+void test_case()
+{
+    unsigned a;
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
