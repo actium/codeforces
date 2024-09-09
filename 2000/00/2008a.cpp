6208369f66bc0ab59eba48f0a6ea2c3251decd2e
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2024-09-09 17:10:10 +0300

    Add solution for problem 2008A

---
+++ 2000/00/2008a.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(unsigned a, unsigned b)
+{
+    if (a % 2 == 1)
+        return answer(false);
+
+    answer(a >= 2 || b % 2 == 0);
+}
+
+void test_case()
+{
+    unsigned a, b;
+    std::cin >> a >> b;
+
+    solve(a, b);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
