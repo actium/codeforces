f7abca9b6e41afce0f7716a8e2c83000f4b50f76
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2024-09-09 17:23:29 +0300

    Add solution for problem 2008B

---
+++ 2000/00/2008b.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(const std::string& s)
+{
+    const size_t n = s.length();
+
+    size_t m = 1;
+    while (m * m < n)
+        ++m;
+
+    if (m * m != n)
+        return answer(false);
+
+    unsigned k = 0;
+    for (const char c : s)
+        k += c - '0';
+
+    answer(k == 4 * m - 4);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::string s;
+    std::cin >> s;
+
+    solve(s);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
