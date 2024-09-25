3497ea19da9dff61176a5b58e2fdf0583fdeb777
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2024-09-25 03:24:27 +0300

    Add solution for problem 2014A

---
+++ 2000/10/2014a.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& a, unsigned k)
+{
+    unsigned q = 0, c = 0;
+    for (const unsigned x : a) {
+        if (x == 0)
+            c += (q > c);
+
+        if (x >= k)
+            q += x;
+    }
+
+    answer(c);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    unsigned k;
+    std::cin >> k;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a, k);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
