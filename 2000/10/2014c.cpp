6ad7fb9462900bf57fef95f6c121ee2b4bd97f59
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2024-09-25 07:57:30 +0300

    Add solution for problem 2014C

---
+++ 2000/10/2014c.cpp
@@ -0,0 +1,63 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using integer = long long;
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(integer x)
+{
+    std::cout << x << '\n';
+}
+
+void no_answer()
+{
+    std::cout << -1 << '\n';
+}
+
+void solve(std::vector<unsigned>& a)
+{
+    const size_t n = a.size();
+    if (n < 3)
+        return no_answer();
+
+    std::sort(a.begin(), a.end());
+
+    integer x = 0;
+    for (const unsigned v : a)
+        x += v;
+
+    answer(std::max(2ll * a[n/2] * n - x + 1, 0ll));
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
