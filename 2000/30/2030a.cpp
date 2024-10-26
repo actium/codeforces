a663dcf1d41176105f85f5f9ae2c860d6f3f56e1
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2024-10-26 10:14:05 +0300

    Add solution for problem 2030A

---
+++ 2000/30/2030a.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    const size_t n = a.size();
+
+    unsigned lb = a[0], ub = a[0];
+    for (size_t i = 1; i < n; ++i) {
+        lb = std::min(lb, a[i]);
+        ub = std::max(ub, a[i]);
+    }
+
+    answer((ub - lb) * (n - 1));
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
