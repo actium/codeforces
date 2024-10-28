72f315cf6283dc5b786348fc89d44c81d3b0ac2d
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2024-10-28 06:04:51 +0300

    Add solution for problem 2021A

---
+++ 2000/20/2021a.cpp
@@ -0,0 +1,53 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(std::vector<unsigned>& a)
+{
+    const size_t n = a.size();
+
+    std::sort(a.begin(), a.end());
+
+    for (size_t i = 1; i < n; ++i)
+        a[i] = (a[i-1] + a[i]) / 2;
+
+    answer(a[n-1]);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
