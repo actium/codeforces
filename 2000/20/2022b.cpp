851e9e520b7dc0a0295486a914c83da4b48d0175
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2024-11-01 00:02:32 +0300

    Add solution for problem 2022B

---
+++ 2000/20/2022b.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+
+using integer = unsigned long long;
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(integer x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& a, unsigned x)
+{
+    integer s = 0, lb = 0;
+    for (const unsigned v : a) {
+        s += v;
+        lb = std::max<unsigned>(lb, v);
+    }
+
+    answer(std::max(lb, (s + x - 1) / x));
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    unsigned x;
+    std::cin >> x;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a, x);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
