7bc4f25e32a8665364e24900e9b471b502c8a11a
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2024-11-05 13:19:40 +0300

    Add solution for problem 2027A

---
+++ 2000/20/2027a.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+
+template <typename T, typename U>
+std::istream& operator >>(std::istream& input, std::pair<T, U>& v)
+{
+    return input >> v.first >> v.second;
+}
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<std::pair<unsigned, unsigned>>& s)
+{
+    unsigned w = 0, h = 0;
+    for (const auto& e : s) {
+        w = std::max(w, e.first);
+        h = std::max(h, e.second);
+    }
+
+    answer(2 * (w + h));
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<std::pair<unsigned, unsigned>> s(n);
+    std::cin >> s;
+
+    solve(s);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
