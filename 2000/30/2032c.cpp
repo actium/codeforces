83a74027dc5cea47d37054a38a16576918796699
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2024-11-07 09:49:16 +0300

    Add solution for problem 2032C

---
+++ 2000/30/2032c.cpp
@@ -0,0 +1,58 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(std::vector<unsigned>& a)
+{
+    const size_t n = a.size();
+
+    std::sort(a.begin(), a.end());
+
+    size_t k = n;
+    for (size_t i = 0, j = 2; j < n; ++j) {
+        while (a[i] + a[i+1] <= a[j])
+            ++i;
+
+        k = std::min(k, i + n - (j + 1));
+    }
+
+    answer(k);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
