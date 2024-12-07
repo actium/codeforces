f371345194f1c3e13675405cd645f32edf22a1f8
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2024-12-07 09:33:30 +0300

    Add solution for problem 2042A

---
+++ 2000/40/2042a.cpp
@@ -0,0 +1,56 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(std::vector<unsigned>& a, unsigned k)
+{
+    std::sort(a.begin(), a.end());
+
+    while (k >= a.back()) {
+        k -= a.back();
+        a.pop_back();
+    }
+
+    answer(k);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    unsigned k;
+    std::cin >> k;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a, k);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
