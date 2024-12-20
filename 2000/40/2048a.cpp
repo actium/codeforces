e569e575c87688160ff03d193995661cce6f68aa
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2024-12-20 11:29:55 +0300

    Add solution for problem 2048A

---
+++ 2000/40/2048a.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(unsigned x)
+{
+    answer(x % 33 == 0);
+}
+
+void test_case()
+{
+    unsigned x;
+    std::cin >> x;
+
+    solve(x);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
