d7907cfcb0fccee52cbafe01000d3ae6b3a27469
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2024-12-22 20:52:07 +0300

    Add solution for problem 2051A

---
+++ 2000/50/2051a.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& a, const std::vector<unsigned>& b)
+{
+    const size_t n = a.size();
+
+    unsigned d = a[n-1];
+    for (size_t i = 1; i < n; ++i) {
+        if (b[i] < a[i-1])
+            d += a[i-1] - b[i];
+    }
+
+    answer(d);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n), b(n);
+    std::cin >> a >> b;
+
+    solve(a, b);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
