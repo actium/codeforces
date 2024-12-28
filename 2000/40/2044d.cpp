d6800ab43632b1f0f8fe7a879e2157057a41b6a9
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2024-12-28 22:46:35 +0300

    Add solution for problem 2044D

---
+++ 2000/40/2044d.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(const std::vector<unsigned>& v)
+{
+    const char* separator = "";
+    for (const unsigned x : v) {
+        std::cout << separator << x;
+        separator = " ";
+    }
+    std::cout << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    const size_t n = a.size();
+
+    std::vector<unsigned> b(n);
+    {
+        std::vector<bool> v(1+n);
+        for (size_t i = 0, p = 1; i < n; ++i) {
+            if (v[a[i]]) {
+                while (v[p])
+                    ++p;
+
+                b[i] = p;
+            } else {
+                b[i] = a[i];
+            }
+
+            v[b[i]] = true;
+        }
+    }
+    answer(b);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
