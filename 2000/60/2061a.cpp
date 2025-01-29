da8be7e0324d8ab9826ece39870517ec8c657adf
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2025-01-29 11:14:40 +0300

    Add solution for problem 2061A

---
+++ 2000/60/2061a.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    unsigned f[2] = {};
+    for (const unsigned x : a)
+        ++f[x%2];
+
+    answer(f[0] == 0 ? f[1] - 1 : 1 + f[1]);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
