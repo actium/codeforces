2ec84ed737db0200f0fdd222ce0630f4e3356648
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2025-02-01 08:35:55 +0300

    Add solution for problem 2056C

---
+++ 2000/50/2056c.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+void answer(const std::vector<unsigned>& v)
+{
+    const char* separator = "";
+    for (const unsigned x : v) {
+        std::cout << separator << x;
+        separator = " ";
+    }
+    std::cout << '\n';
+}
+
+void solve(unsigned n)
+{
+    std::vector<unsigned> a(n, 1);
+    for (size_t i = 1; i < n; ++i)
+        a[i] += (i - 1) % (n / 2);
+
+    answer(a);
+}
+
+void test_case()
+{
+    unsigned n;
+    std::cin >> n;
+
+    solve(n);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
