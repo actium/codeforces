41c45d75a64547e1a16d73ae2ca9c1be7bc672f5
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2025-02-20 14:33:43 +0300

    Add solution for problem 2065B

---
+++ 2000/60/2065b.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::string& s)
+{
+    const size_t n = s.length();
+
+    for (size_t i = 1; i < n; ++i) {
+        if (s[i] == s[i-1])
+            return answer(1);
+    }
+
+    answer(n);
+}
+
+void test_case()
+{
+    std::string s;
+    std::cin >> s;
+
+    solve(s);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
