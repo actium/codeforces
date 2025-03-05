19ee8d10b2c0159e00386205b3d21bff6b05f166
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2025-03-05 19:05:11 +0300

    Add solution for problem 2072B

---
+++ 2000/70/2072b.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+
+using integer = unsigned long long;
+
+void answer(integer x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::string& s)
+{
+    unsigned f[2] = {};
+    for (const char c : s)
+        ++f[c=='_'];
+
+    const integer a = f[0] / 2, b = f[0] - a;
+
+    answer(a * f[1] * b);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::string s;
+    std::cin >> s;
+
+    solve(s);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
