ee578c887fe84a3386eaf6fe28744929a630ab7f
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2025-03-19 07:32:46 +0300

    Add solution for problem 2078A

---
+++ 2000/70/2078a.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(const std::vector<unsigned>& a, unsigned x)
+{
+    unsigned s = 0;
+    for (const unsigned v : a)
+        s += v;
+
+    answer(s == x * a.size());
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    unsigned x;
+    std::cin >> x;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a, x);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
