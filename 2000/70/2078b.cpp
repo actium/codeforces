31df77703a74fc1cd534f09a6af0fe7c2c7bc694
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2025-03-26 15:26:09 +0300

    Add solution for problem 2078B

---
+++ 2000/70/2078b.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+void answer(const std::vector<unsigned>& v)
+{
+    const char* separator = "";
+    for (const unsigned x : v) {
+        std::cout << separator << x;
+        separator = " ";
+    }
+    std::cout << '\n';
+}
+
+void solve(unsigned n, unsigned k)
+{
+    std::vector<unsigned> t(n, n - 1 + k % 2);
+    t[n-2] = n;
+    t[n-1] = n - 1;
+
+    answer(t);
+}
+
+void test_case()
+{
+    unsigned n, k;
+    std::cin >> n >> k;
+
+    solve(n, k);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
