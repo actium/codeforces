4396c0ce75ca077c6ae7ed23809602d4569aed0f
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2025-03-29 08:00:10 +0300

    Add solution for problem 2091C

---
+++ 2000/90/2091c.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+
+void answer(const std::vector<unsigned>& v)
+{
+    const char* separator = "";
+    for (const unsigned x : v) {
+        std::cout << separator << x;
+        separator = " ";
+    }
+    std::cout << '\n';
+}
+
+void no_answer()
+{
+    std::cout << -1 << '\n';
+}
+
+void solve(size_t n)
+{
+    if (n % 2 == 0)
+        return no_answer();
+
+    std::vector<unsigned> p(n);
+    for (size_t i = 0; i < n; ++i)
+        p[i] = n - i;
+
+    answer(p);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    solve(n);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
