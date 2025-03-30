4ba703d960588696eedd1878d715481b31b8f2ca
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2025-03-30 10:07:28 +0300

    Add solution for problem 2075A

---
+++ 2000/70/2075a.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(unsigned n, unsigned k)
+{
+    answer((n - n % 2 + k - 2) / (k - 1));
+}
+
+void test_case()
+{
+    unsigned n, k;
+    std::cin >> n >> k;
+
+    solve(n, k);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
