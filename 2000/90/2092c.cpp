662927aa85319635f4baff239cd877dddd42326e
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2025-04-10 07:13:01 +0300

    Add solution for problem 2092C

---
+++ 2000/90/2092c.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+
+using integer = unsigned long long;
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(integer x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    integer sum = 0;
+    unsigned f[2] = {}, max = 0;
+    for (const unsigned x : a) {
+        sum += x;
+        ++f[x%2];
+        max = std::max(max, x);
+    }
+
+    answer(f[0] == 0 || f[1] == 0 ? max : sum - f[1] + 1);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
