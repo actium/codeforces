1df6010f693460d5154ee2f8e0c7a86874a2ef19
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2025-04-23 05:05:06 +0300

    Add solution for problem 2053D

---
+++ 2000/50/2053d.cpp
@@ -0,0 +1,337 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+#include <cassert>
+
+namespace modular_arithmetic {
+
+    template <unsigned M>
+    class Integer {
+        static_assert(M != 0, "modulus must be non-zero");
+
+    public:
+        static constexpr unsigned modulus = M;
+
+        constexpr Integer() noexcept
+            : residue_(0)
+        {}
+
+        constexpr Integer(long long value) noexcept
+            : residue_(value < 0 ? value % modulus + modulus : value % modulus)
+        {}
+
+        [[nodiscard]] constexpr operator unsigned() const noexcept
+        {
+            return residue_;
+        }
+
+        constexpr Integer& operator +=(const Integer& rhs) noexcept
+        {
+            if (modulus - residue_ <= rhs.residue_)
+                residue_ -= modulus;
+
+            residue_ += rhs.residue_;
+            return *this;
+        }
+
+        constexpr Integer& operator -=(const Integer& rhs) noexcept
+        {
+            if (residue_ < rhs.residue_)
+                residue_ += modulus;
+
+            residue_ -= rhs.residue_;
+            return *this;
+        }
+
+        constexpr Integer& operator *=(const Integer& rhs) noexcept
+        {
+            residue_ = static_cast<uint64_t>(residue_) * rhs.residue_ % modulus;
+            return *this;
+        }
+
+        constexpr Integer& operator /=(const Integer& rhs) noexcept
+        {
+            return *this *= rhs.invert();
+        }
+
+        constexpr Integer& operator ++() noexcept
+        {
+            if (++residue_ == modulus)
+                residue_ = 0;
+
+            return *this;
+        }
+
+        constexpr Integer& operator --() noexcept
+        {
+            if (residue_-- == 0)
+                residue_ = modulus - 1;
+
+            return *this;
+        }
+
+        constexpr void swap(Integer& other) noexcept
+        {
+            std::swap(residue_, other.residue_);
+        }
+
+        [[nodiscard]] constexpr Integer raise(long long power) const noexcept
+        {
+            if (power < 0) {
+                assert(residue_ != 0);
+                power = static_cast<uint64_t>(-power) * (modulus - 2) % (modulus - 1);
+            }
+
+            Integer result = 1;
+            for (Integer base = *this; power != 0; power >>= 1) {
+                if ((power & 1) != 0)
+                    result *= base;
+
+                base *= base;
+            }
+            return result;
+        }
+
+        // note: if modulus is a prime number, raise(modulus-2) also returns the inverse
+        [[nodiscard]] constexpr Integer invert() const noexcept
+        {
+            assert(residue_ != 0);
+
+            int64_t x = 1, y = 0, a = residue_, b = modulus;
+            while (b != 0) {
+                x = std::exchange(y, x - a / b * y);
+                a = std::exchange(b, a % b);
+            }
+            assert(a == 1 && "inverse does not exist (residue is not coprime to modulus)");
+            return x;
+        }
+
+    private:
+        unsigned residue_;
+
+    }; // class Integer<M>
+
+    // returns true if the operands are congruent modulo M, false otherwise
+    template <unsigned M>
+    constexpr bool operator ==(const Integer<M>& lhs, const Integer<M>& rhs) noexcept
+    {
+        return static_cast<unsigned>(lhs) == static_cast<unsigned>(rhs);
+    }
+
+    // returns true if the operands are not congruent modulo M, false otherwise
+    template <unsigned M>
+    constexpr bool operator !=(const Integer<M>& lhs, const Integer<M>& rhs) noexcept
+    {
+        return static_cast<unsigned>(lhs) != static_cast<unsigned>(rhs);
+    }
+
+    template <unsigned M>
+    constexpr Integer<M> operator +(const Integer<M>& number) noexcept
+    {
+        return number;
+    }
+
+    template <unsigned M>
+    constexpr Integer<M> operator -(const Integer<M>& number) noexcept
+    {
+        return M - static_cast<unsigned>(number);
+    }
+
+    template <unsigned M>
+    constexpr Integer<M> operator +(const Integer<M>& lhs, const Integer<M>& rhs) noexcept
+    {
+        return Integer<M>(lhs) += rhs;
+    }
+
+    template <typename T, unsigned M>
+    constexpr Integer<M> operator +(const Integer<M>& lhs, T rhs) noexcept
+    {
+        return Integer<M>(lhs) += rhs;
+    }
+
+    template <typename T, unsigned M>
+    constexpr Integer<M> operator +(T&& lhs, const Integer<M>& rhs) noexcept
+    {
+        return Integer<M>(lhs) += rhs;
+    }
+
+    template <unsigned M>
+    constexpr Integer<M> operator -(const Integer<M>& lhs, const Integer<M>& rhs) noexcept
+    {
+        return Integer<M>(lhs) -= rhs;
+    }
+
+    template <typename T, unsigned M>
+    constexpr Integer<M> operator -(const Integer<M>& lhs, T rhs) noexcept
+    {
+        return Integer<M>(lhs) -= rhs;
+    }
+
+    template <typename T, unsigned M>
+    constexpr Integer<M> operator -(T&& lhs, const Integer<M>& rhs) noexcept
+    {
+        return Integer<M>(lhs) -= rhs;
+    }
+
+    template <unsigned M>
+    constexpr Integer<M> operator *(const Integer<M>& lhs, const Integer<M>& rhs) noexcept
+    {
+        return Integer<M>(lhs) *= rhs;
+    }
+
+    template <typename T, unsigned M>
+    constexpr Integer<M> operator *(const Integer<M>& lhs, T rhs) noexcept
+    {
+        return Integer<M>(lhs) *= rhs;
+    }
+
+    template <typename T, unsigned M>
+    constexpr Integer<M> operator *(T&& lhs, const Integer<M>& rhs) noexcept
+    {
+        return Integer<M>(lhs) *= rhs;
+    }
+
+    template <unsigned M>
+    constexpr Integer<M> operator /(const Integer<M>& lhs, const Integer<M>& rhs) noexcept
+    {
+        return Integer<M>(lhs) /= rhs;
+    }
+
+    template <typename T, unsigned M>
+    constexpr Integer<M> operator /(const Integer<M>& lhs, T rhs) noexcept
+    {
+        return Integer<M>(lhs) /= rhs;
+    }
+
+    template <typename T, unsigned M>
+    constexpr Integer<M> operator /(T&& lhs, const Integer<M>& rhs) noexcept
+    {
+        return Integer<M>(lhs) /= rhs;
+    }
+
+    template <unsigned M>
+    constexpr Integer<M> operator ++(Integer<M>& number, int) noexcept
+    {
+        const Integer<M> s = number;
+        ++number;
+        return s;
+    }
+
+    template <unsigned M>
+    constexpr Integer<M> operator --(Integer<M>& number, int) noexcept
+    {
+        const Integer<M> s = number;
+        --number;
+        return s;
+    }
+
+    template <unsigned M>
+    std::istream& operator >>(std::istream& input, Integer<M>& number)
+    {
+        long long value;
+        input >> value;
+        number = value;
+        return input;
+    }
+
+    template <unsigned M>
+    std::ostream& operator <<(std::ostream& output, const Integer<M>& number)
+    {
+        return output << static_cast<unsigned>(number);
+    }
+
+} // namespace modular_arithmetic
+
+using integer = modular_arithmetic::Integer<998244353>;
+
+template <typename T, typename U>
+std::istream& operator >>(std::istream& input, std::pair<T, U>& v)
+{
+    return input >> v.first >> v.second;
+}
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(const std::vector<unsigned>& v)
+{
+    const char* separator = "";
+    for (const unsigned x : v) {
+        std::cout << separator << x;
+        separator = " ";
+    }
+    std::cout << '\n';
+}
+
+void solve(std::vector<unsigned>& a, std::vector<unsigned>& b, const std::vector<std::pair<unsigned, unsigned>>& r)
+{
+    const size_t n = a.size(), q = r.size();
+
+    std::vector<unsigned> s[2] = { a, b };
+    for (size_t i = 0; i < 2; ++i)
+        std::sort(s[i].begin(), s[i].end());
+
+    const auto update = [&](size_t t, size_t x) {
+        std::vector<unsigned>* const c[2] = { &a, &b };
+
+        const size_t p = std::upper_bound(s[t].begin(), s[t].end(), (*c[t])[x]++) - s[t].begin() - 1;
+
+        const unsigned old_value = std::min(s[0][p], s[1][p]);
+        ++s[t][p];
+        const unsigned new_value = std::min(s[0][p], s[1][p]);
+
+        return std::make_pair(old_value, new_value);
+    };
+
+    integer p = 1;
+    for (size_t i = 0; i < n; ++i)
+        p *= std::min(s[0][i], s[1][i]);
+
+    std::vector<unsigned> v(1 + q);
+    v[0] = p;
+    for (size_t i = 0; i < q; ++i) {
+        const std::pair<unsigned, unsigned> d = update(r[i].first-1, r[i].second-1);
+        if (d.second != d.first) {
+            p /= d.first;
+            p *= d.second;
+        }
+
+        v[i+1] = p;
+    }
+
+    answer(v);
+}
+
+void test_case()
+{
+    size_t n, q;
+    std::cin >> n >> q;
+
+    std::vector<unsigned> a(n), b(n);
+    std::cin >> a >> b;
+
+    std::vector<std::pair<unsigned, unsigned>> r(q);
+    std::cin >> r;
+
+    solve(a, b, r);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
