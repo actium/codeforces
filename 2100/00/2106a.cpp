c3aecb9363a0c9130bfaf2a2ced4fafe72e6625b
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2025-05-15 06:14:14 +0300

    Add solution for problem 2106A

---
+++ 2100/00/2106a.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::string& s)
+{
+    const size_t n = s.size();
+
+    unsigned k = 0;
+    for (const char c : s)
+        k += (c == '0' ? 1 : n - 1);
+
+    answer(k);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::string s;
+    std::cin >> s;
+
+    solve(s);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
