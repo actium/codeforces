2b7994ecb48719e1acb253d5d659df094ca80b4c
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2025-05-17 13:52:46 +0300

    Add solution for problem 2104A

---
+++ 2100/00/2104a.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(unsigned a, unsigned b, unsigned c)
+{
+    const unsigned t = a + b + c;
+
+    answer(t % 3 == 0 && a <= t / 3 && b <= t / 3);
+}
+
+void test_case()
+{
+    unsigned a, b, c;
+    std::cin >> a >> b >> c;
+
+    solve(a, b, c);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
