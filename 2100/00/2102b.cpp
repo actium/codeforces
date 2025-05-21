52111840c25e4fd07e40439ca6e772df295a4be2
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2025-05-21 05:14:30 +0300

    Add solution for problem 2102B

---
+++ 2100/00/2102b.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(const std::vector<int>& a)
+{
+    const size_t n = a.size();
+
+    unsigned c = 0;
+    for (size_t i = 1; i < n; ++i)
+        c += (std::abs(a[i]) > std::abs(a[0]));
+
+    answer(c >= (n - 1) / 2);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<int> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
