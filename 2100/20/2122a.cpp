93eb38ba337c23b04e370774279fce6022218866
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2025-07-22 11:25:30 +0300

    Add solution for problem 2122A

---
+++ 2100/20/2122a.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(unsigned n, unsigned m)
+{
+    answer((n - 1) * (m - 1) > 1);
+}
+
+void test_case()
+{
+    unsigned n, m;
+    std::cin >> n >> m;
+
+    solve(n, m);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
