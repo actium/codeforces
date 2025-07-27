0f1d6813599bd5482c1f61a40894dc3a9447f92d
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2025-07-27 08:20:22 +0300

    Add solution for problem 2125A

---
+++ 2100/20/2125a.cpp
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+
+void answer(const std::string& x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(std::string& s)
+{
+    std::sort(s.begin(), s.end(), std::greater<char>());
+
+    answer(s);
+}
+
+void test_case()
+{
+    std::string s;
+    std::cin >> s;
+
+    solve(s);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
