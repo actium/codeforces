283dd61506a6993f942b525bea0387ef1bbb9e1c
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2025-08-11 03:51:36 +0300

    Add solution for problem 2131A

---
+++ 2100/30/2131a.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& a, const std::vector<unsigned>& b)
+{
+    unsigned k = 1;
+    for (auto it = a.begin(), jt = b.begin(); it != a.end(); ++it, ++jt) {
+        if (*it > *jt)
+            k += *it - *jt;
+    }
+
+    answer(k);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n), b(n);
+    std::cin >> a >> b;
+
+    solve(a, b);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
