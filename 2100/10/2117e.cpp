f8f2042f159e7817e3689a71ea48d66e997f5703
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2025-08-22 04:37:48 +0300

    Add solution for problem 2117E

---
+++ 2100/10/2117e.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& a, const std::vector<unsigned>& b)
+{
+    const size_t n = a.size();
+
+    std::vector<size_t> p(1 + n), q(1 + n);
+    for (size_t i = 1; i < n; ++i) {
+        q[a[i]] = i - 1;
+        p[a[i]] = i;
+
+        p[b[i]] = i - 1;
+        q[b[i]] = i;
+    }
+
+    unsigned k = 0;
+    for (size_t i = 0; i < n; ++i) {
+        if (a[i] == b[i] || p[a[i]] > i || q[b[i]] > i)
+            k = i + 1;
+    }
+
+    answer(k);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n), b(n);
+    std::cin >> a >> b;
+
+    solve(a, b);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
