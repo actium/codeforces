fcb0f01d014e13141096faac842966446adaa59d
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2025-09-01 06:40:52 +0300

    Add solution for problem 2134B

---
+++ 2100/30/2134b.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+
+using integer = unsigned long long;
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(const std::vector<integer>& v)
+{
+    const char* separator = "";
+    for (const integer x : v) {
+        std::cout << separator << x;
+        separator = " ";
+    }
+    std::cout << '\n';
+}
+
+void solve(const std::vector<unsigned>& a, unsigned k)
+{
+    const size_t n = a.size();
+
+    std::vector<integer> b(n);
+    for (size_t i = 0; i < n; ++i)
+        b[i] = a[i] + integer(a[i] % (k + 1)) * k;
+
+    answer(b);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    unsigned k;
+    std::cin >> k;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a, k);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
