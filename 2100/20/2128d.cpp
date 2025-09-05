fd8d57f661060bc626f7b5130b6cd7e10dcee9d1
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2025-09-05 06:06:46 +0300

    Add solution for problem 2128D

---
+++ 2100/20/2128d.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+
+using integer = unsigned long long;
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(integer x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& p)
+{
+    const integer n = p.size();
+
+    integer k = n * (n + 1) * (n + 2) / 6;
+    for (size_t i = 1; i < n; ++i) {
+        if (p[i-1] < p[i])
+            k -= i * (n - i);
+    }
+
+    answer(k);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> p(n);
+    std::cin >> p;
+
+    solve(p);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
