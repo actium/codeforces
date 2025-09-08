8ebb80daf90e20336bdb69c4db1da96d468b163d
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2025-09-08 03:25:10 +0300

    Add solution for problem 2129B

---
+++ 2100/20/2129b.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& p)
+{
+    const size_t n = p.size();
+
+    unsigned k = 0;
+    for (size_t i = 0; i < n; ++i) {
+        unsigned c = 0;
+        for (size_t j = 0; j < i; ++j)
+            c += (p[j] > p[i]);
+
+        unsigned d = 0;
+        for (size_t j = i + 1; j < n; ++j)
+            d += (p[j] > p[i]);
+
+        k += std::min(c, d);
+    }
+
+    answer(k);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> p(n);
+    std::cin >> p;
+
+    solve(p);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
