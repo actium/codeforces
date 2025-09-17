bfe1d1265690f53ee5674b154dae3c7520f8d123
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2025-09-17 04:46:13 +0300

    Add solution for problem 2139A

---
+++ 2100/30/2139a.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(unsigned a, unsigned b)
+{
+    if (a == b)
+        return answer(0);
+
+    answer(a % b == 0 || b % a == 0 ? 1 : 2);
+}
+
+void test_case()
+{
+    unsigned a, b;
+    std::cin >> a >> b;
+
+    solve(a, b);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
