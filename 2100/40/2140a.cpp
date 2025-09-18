c25b72051ba742abc73f2b4c9a034e4810401700
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2025-09-18 04:42:53 +0300

    Add solution for problem 2140A

---
+++ 2100/40/2140a.cpp
@@ -0,0 +1,38 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::string& s)
+{
+    const auto z = std::count(s.begin(), s.end(), '0');
+    answer(std::count(s.begin(), s.begin() + z, '1'));
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::string s;
+    std::cin >> s;
+
+    solve(s);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
