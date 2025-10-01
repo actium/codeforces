3a41ad1d22e5e8d739b667a5bfe9797ec01d32ba
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2025-10-01 04:18:01 +0300

    Add solution for problem 2147A

---
+++ 2100/40/2147a.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void no_answer()
+{
+    std::cout << -1 << '\n';
+}
+
+void solve(unsigned x, unsigned y)
+{
+    if (y > x)
+        return answer(2);
+
+    if (y + 1 < x && y != 1)
+        return answer(3);
+
+    no_answer();
+}
+
+void test_case()
+{
+    unsigned x, y;
+    std::cin >> x >> y;
+
+    solve(x, y);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
