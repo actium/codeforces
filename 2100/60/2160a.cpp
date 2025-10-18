37945b453a23490fa17eaff2fd6a93e1cbf0d88e
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2025-10-18 05:33:19 +0300

    Add solution for problem 2160A

---
+++ 2100/60/2160a.cpp
@@ -0,0 +1,52 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(std::vector<unsigned>& a)
+{
+    std::sort(a.begin(), a.end());
+
+    unsigned x = 0;
+    for (const unsigned v : a)
+        x += (v == x);
+
+    answer(x);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
