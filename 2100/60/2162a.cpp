cefad68d00e7587f3470e07fd3a9b5268b5359d8
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2025-10-21 18:05:46 +0300

    Add solution for problem 2162A

---
+++ 2100/60/2162a.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    unsigned v = 0;
+    for (const unsigned x : a)
+        v = std::max(v, x);
+
+    answer(v);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
