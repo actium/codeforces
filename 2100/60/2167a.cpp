064ce8f4dd3682efafe61e9212f0936ad926050c
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2025-11-04 05:39:55 +0300

    Add solution for problem 2167A

---
+++ 2100/60/2167a.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(unsigned a, unsigned b, unsigned c, unsigned d)
+{
+    answer(a == b && b == c && c == d);
+}
+
+void test_case()
+{
+    unsigned a, b, c, d;
+    std::cin >> a >> b >> c >> d;
+
+    solve(a, b, c, d);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
