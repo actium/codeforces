2d7f64cf65b349de0548cb38f2fc89c3100e2a54
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2025-11-19 05:55:24 +0300

    Add solution for problem 2164B

---
+++ 2100/60/2164b.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x, unsigned y)
+{
+    std::cout << x << ' ' << y << '\n';
+}
+
+void no_answer()
+{
+    std::cout << -1 << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    const size_t n = a.size();
+
+    for (size_t i = 1; i < n; ++i) {
+        for (size_t j = 0; j < i; ++j) {
+            if (a[i] % a[j] % 2 == 0)
+                return answer(a[j], a[i]);
+        }
+    }
+
+    no_answer();
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
