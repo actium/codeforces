9c9f07916fca84996341e2a89ce9880faa867870
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2025-11-24 07:58:02 +0300

    Add solution for problem 2157A

---
+++ 2100/50/2157a.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    const size_t n = a.size();
+
+    std::vector<unsigned> f(1+n);
+    for (const unsigned x : a)
+        ++f[x];
+
+    unsigned k = 0;
+    for (size_t i = 0; i <= n; ++i)
+        k += (f[i] < i ? f[i] : f[i] - i);
+
+    answer(k);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
