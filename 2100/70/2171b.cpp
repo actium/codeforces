092bbefa225b304a8d6a10226af62ecaa1efc151
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2025-11-25 01:28:01 +0300

    Add solution for problem 2171B

---
+++ 2100/70/2171b.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned u, const std::vector<int>& v)
+{
+    std::cout << u << '\n';
+
+    const char* separator = "";
+    for (const int x : v) {
+        std::cout << separator << x;
+        separator = " ";
+    }
+    std::cout << '\n';
+}
+
+void solve(std::vector<int>& a)
+{
+    if (a.front() == -1)
+        a.front() = a.back();
+    if (a.back() == -1)
+        a.back() = a.front();
+
+    for (int& x : a) {
+        if (x == -1)
+            x = 0;
+    }
+
+    answer(abs(a.back() - a.front()), a);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<int> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
