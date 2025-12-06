1609af10e105dce7af56b5f5868a0145b627ec9a
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2025-12-06 05:52:57 +0300

    Add solution for problem 2173A

---
+++ 2100/70/2173a.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::string& s, size_t k)
+{
+    const size_t n = s.length();
+
+    unsigned q = 0;
+    for (size_t i = 0, x = 0; i < n; ++i) {
+        if (s[i] == '0')
+            q += (i >= x);
+        else
+            x = i + k + 1;
+    }
+
+    answer(q);
+}
+
+void test_case()
+{
+    size_t n, k;
+    std::cin >> n >> k;
+
+    std::string s;
+    std::cin >> s;
+
+    solve(s, k);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
