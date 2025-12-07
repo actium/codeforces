f319dfc2ea44fa0e25315b955fa9c7535de17dea
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2025-12-07 06:16:21 +0300

    Add solution for problem 2175A

---
+++ 2100/70/2175a.cpp
@@ -0,0 +1,49 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(std::vector<unsigned>& a)
+{
+    std::sort(a.begin(), a.end());
+
+    const auto e = std::unique(a.begin(), a.end());
+    answer(*std::lower_bound(a.begin(), e, e - a.begin()));
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
