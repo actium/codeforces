bf82c5944e722f9668e766fbd79fecf203bd3436
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2026-01-08 00:02:49 +0300

    Add solution for problem 2179C

---
+++ 2100/70/2179c.cpp
@@ -0,0 +1,48 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(std::vector<unsigned>& a)
+{
+    std::nth_element(a.begin(), a.begin() + 1, a.end());
+
+    answer(std::max(a[0], a[1] - a[0]));
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
