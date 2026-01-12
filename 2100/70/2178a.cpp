b63fe545193db95f063b3f24ef7f63042ec91973
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2026-01-12 04:59:54 +0300

    Add solution for problem 2178A

---
+++ 2100/70/2178a.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(const std::string& s)
+{
+    answer(s.find('Y') == s.rfind('Y'));
+}
+
+void test_case()
+{
+    std::string s;
+    std::cin >> s;
+
+    solve(s);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
