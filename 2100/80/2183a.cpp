b8ee92d6a033dfb7f8d42a1c515127601795bd5b
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2026-01-17 04:30:25 +0300

    Add solution for problem 2183A

---
+++ 2100/80/2183a.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "Alice", "Bob" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    answer(a.front() == 0 && a.back() == 0);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
