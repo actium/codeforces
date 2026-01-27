6418c3946d76652ff761dfaa02eda031f2499b57
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2026-01-27 06:37:44 +0300

    Add solution for problem 2193B

---
+++ 2100/90/2193b.cpp
@@ -0,0 +1,59 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(const std::vector<unsigned>& v)
+{
+    const char* separator = "";
+    for (const unsigned x : v) {
+        std::cout << separator << x;
+        separator = " ";
+    }
+    std::cout << '\n';
+}
+
+void solve(std::vector<unsigned>& p)
+{
+    auto it = p.begin();
+    for (unsigned x = p.size(); it != p.end() && *it == x; ++it, --x);
+
+    if (it != p.end()) {
+        const auto jt = std::max_element(it, p.end());
+        std::reverse(it, std::next(jt));
+    }
+
+    answer(p);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> p(n);
+    std::cin >> p;
+
+    solve(p);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
