c0cbb3b1e99a6dac560b6235abedb589bbde5c48
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2026-01-30 06:37:08 +0300

    Add solution for problem 2188A

---
+++ 2100/80/2188a.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+void answer(const std::vector<unsigned>& v)
+{
+    const char* separator = "";
+    for (const unsigned x : v) {
+        std::cout << separator << x;
+        separator = " ";
+    }
+    std::cout << '\n';
+}
+
+void solve(unsigned n)
+{
+    std::vector<unsigned> p(n);
+    for (unsigned i = 1; i <= n; ++i)
+        p[n-i] = (i % 2 == 1 ? n - i / 2 : i / 2);
+
+    answer(p);
+}
+
+void test_case()
+{
+    unsigned n;
+    std::cin >> n;
+
+    solve(n);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
