d810e8b9ebe768006819b7077091df9f60aeee0b
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2026-03-01 07:57:02 +0300

    Add solution for problem 2205A

---
+++ 2200/00/2205a.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(const std::vector<unsigned>& v)
+{
+    const char* separator = "";
+    for (const unsigned x : v) {
+        std::cout << separator << x;
+        separator = " ";
+    }
+    std::cout << '\n';
+}
+
+void solve(std::vector<unsigned>& p)
+{
+    const size_t n = p.size();
+
+    for (size_t i = 0; i < n; ++i) {
+        if (p[i] == n)
+            std::swap(p[0], p[i]);
+    }
+
+    answer(p);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> p(n);
+    std::cin >> p;
+
+    solve(p);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
