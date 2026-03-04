1bc40b67886bd403fbc3115651b24926bffdf8d4
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2026-03-05 00:21:42 +0300

    Add solution for problem 2200C

---
+++ 2200/00/2200c.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(const std::string& s)
+{
+    std::string t;
+    for (const char c : s) {
+        if (!t.empty() && t.back() == c)
+            t.pop_back();
+        else
+            t.push_back(c);
+    }
+
+    answer(t.empty());
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::string s;
+    std::cin >> s;
+
+    solve(s);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
