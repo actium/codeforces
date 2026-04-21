38b718b9f184bbf7b899de225b8670427e52c504
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2026-04-21 08:10:07 +0300

    Add solution for problem 2220A

---
+++ 2200/20/2220a.cpp
@@ -0,0 +1,61 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(const std::vector<unsigned>& v)
+{
+    const char* separator = "";
+    for (const unsigned x : v) {
+        std::cout << separator << x;
+        separator = " ";
+    }
+    std::cout << '\n';
+}
+
+void no_answer()
+{
+    std::cout << -1 << '\n';
+}
+
+void solve(std::vector<unsigned>& a)
+{
+    std::sort(a.begin(), a.end(), std::greater<unsigned>());
+
+    if (std::unique(a.begin(), a.end()) == a.end())
+        return answer(a);
+
+    no_answer();
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
