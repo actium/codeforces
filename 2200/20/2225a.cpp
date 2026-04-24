a7fb7ffe43c77ff7a9482d80b5256121f229471a
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2026-04-24 05:52:33 +0300

    Add solution for problem 2225A

---
+++ 2200/20/2225a.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using integer = unsigned long long;
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(integer x, integer y)
+{
+    answer(y - x > x);
+}
+
+void test_case()
+{
+    integer x, y;
+    std::cin >> x >> y;
+
+    solve(x, y);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
