572b8b0dc52a1db7859bdabc8266bc4d5b37affe
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2026-04-24 05:58:08 +0300

    Add solution for problem 2225B

---
+++ 2200/20/2225b.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(const std::string& s)
+{
+    const size_t n = s.length();
+
+    unsigned k = 0;
+    for (size_t i = 1; i < n; ++i)
+        k += (s[i] == s[i-1]);
+
+    answer(k < 3);
+}
+
+void test_case()
+{
+    std::string s;
+    std::cin >> s;
+
+    solve(s);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
