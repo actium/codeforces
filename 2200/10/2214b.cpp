0f0ca134e98a582048fdf820b41679a68573b1a3
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2026-04-29 06:00:34 +0300

    Add solution for problem 2214B

---
+++ 2200/10/2214b.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    std::string s;
+    std::cin >> s;
+
+    std::cout << "1F601" << '\n';
+
+    return 0;
+}
