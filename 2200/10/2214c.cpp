a760d76d27fd6cf4a5df417f7cbea93b02f8bdc4
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2026-04-30 07:07:25 +0300

    Add solution for problem 2214C

---
+++ 2200/10/2214c.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+void answer(int x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(unsigned a, unsigned b, unsigned c)
+{
+    if (a > b)
+        std::swap(a, b);
+    if (b > c)
+        std::swap(b, c);
+    if (a > b)
+        std::swap(a, b);
+
+    answer((a ^ b ^ c) - b);
+}
+
+void test_case()
+{
+    unsigned a, b, c;
+    std::cin >> a >> b >> c;
+
+    solve(a, b, c);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
