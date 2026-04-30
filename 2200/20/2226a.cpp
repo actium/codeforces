9d131b6f133a96a3a8dff77c2927776d15130a9f
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2026-05-01 01:27:20 +0300

    Add solution for problem 2226A

---
+++ 2200/20/2226a.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    unsigned c = 0;
+    for (const unsigned x : a) {
+        if (x != 1)
+            c += x;
+    }
+
+    answer(c + (a.back() == 1));
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
