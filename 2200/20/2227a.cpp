ee8203f0588214b095fcbb867bfc97ec58fc1dd1
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2026-05-02 05:08:28 +0300

    Add solution for problem 2227A

---
+++ 2200/20/2227a.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+void answer(bool v)
+{
+    constexpr const char* s[2] = { "NO", "YES" };
+    std::cout << s[v] << '\n';
+}
+
+void solve(unsigned x, unsigned y)
+{
+    answer(x % 2 != 1 || y % 2 != 1);
+}
+
+void test_case()
+{
+    unsigned x, y;
+    std::cin >> x >> y;
+
+    solve(x, y);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
