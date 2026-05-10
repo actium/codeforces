b0b8ac1afffb92d0476fd46e0450795859742cb3
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2026-05-10 10:19:52 +0300

    Add solution for problem 2224B

---
+++ 2200/20/2224b.cpp
@@ -0,0 +1,60 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using integer = unsigned long long;
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(integer x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(std::vector<unsigned>& a)
+{
+    std::sort(a.begin(), a.end());
+
+    unsigned x = 0;
+    for (const unsigned v : a)
+        x += (v == x);
+
+    integer v = (x == a.back() + 1);
+    for (unsigned i = 0; i < a.size(); ++i) {
+        v += a.back();
+        v += std::min(i, x);
+    }
+
+    answer(v);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
