c35113a9801d1690c6de25083601e83b13fe635e
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2026-05-26 06:37:31 +0300

    Add solution for problem 2109C3

---
+++ 2100/00/2109c3.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+
+#include <cassert>
+
+using integer = long long;
+
+constexpr integer B = 1000000000000000000;
+
+void query_digit()
+{
+    std::cout << "digit" << std::endl;
+
+    unsigned r;
+    std::cin >> r;
+}
+
+bool query_add(integer y)
+{
+    std::cout << "add " << y << std::endl;
+
+    unsigned r;
+    std::cin >> r;
+    return r == 1;
+}
+
+bool query_mul(integer y)
+{
+    std::cout << "mul " << y << std::endl;
+
+    unsigned r;
+    std::cin >> r;
+    return r == 1;
+}
+
+void answer()
+{
+    std::cout << "!" << std::endl;
+
+    int r;
+    std::cin >> r;
+}
+
+void solve(unsigned n)
+{
+    query_mul(999999999);
+    query_digit();
+
+    if (n != 81)
+        query_add(n - 81LL);
+
+    answer();
+}
+
+void test_case()
+{
+    unsigned n;
+    std::cin >> n;
+
+    solve(n);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
