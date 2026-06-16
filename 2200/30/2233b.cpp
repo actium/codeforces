38f043655cb48dc00830b42fd52a15b001f0e195
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2026-06-16 06:43:51 +0300

    Add solution for problem 2233B

---
+++ 2200/30/2233b.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+
+void answer(const std::vector<unsigned>& v)
+{
+    const char* separator = "";
+    for (const unsigned x : v) {
+        std::cout << separator << x;
+        separator = " ";
+    }
+    std::cout << '\n';
+}
+
+void solve(unsigned n)
+{
+    std::vector<unsigned> a;
+    a.push_back(n);
+    for (unsigned i = 1; i <= n; ++i) {
+        a.push_back(i);
+        a.push_back(i);
+    }
+    for (unsigned i = 1; i <= n; ++i)
+        a.push_back(i);
+    for (unsigned i = 1; i < n; ++i)
+        a.push_back(i);
+
+    answer(a);
+}
+
+void test_case()
+{
+    unsigned n;
+    std::cin >> n;
+
+    solve(n);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
