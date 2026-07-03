39bfc3cd1beeb80682face24c16e3970f14f4ab5
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2026-07-03 17:07:19 +0700

    Add solution for problem 2241B

---
+++ 2200/40/2241b.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(unsigned x)
+{
+    unsigned y = 1;
+    while (x != 0) {
+        y *= 10;
+        x /= 10;
+    }
+
+    answer(y + 1);
+}
+
+void test_case()
+{
+    unsigned x;
+    std::cin >> x;
+
+    solve(x);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
