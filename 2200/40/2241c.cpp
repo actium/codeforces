63db42b9724d433b8f5f6584ca0e07b266b56344
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2026-07-04 12:07:32 +0700

    Add solution for problem 2241C

---
+++ 2200/40/2241c.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::string& s)
+{
+    const auto x = s.find(s[0] ^ 1);
+
+    if (x == std::string::npos || s.find(s[0], x) != std::string::npos)
+        return answer(1);
+
+    answer(2);
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::string s;
+    std::cin >> s;
+
+    solve(s);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
