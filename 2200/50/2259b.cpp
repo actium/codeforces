e0335f4dba8202b84a7da9bee23126fa0cc3e818
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2026-09-07 06:22:00 +0300

    Add solution for problem 2259B

---
+++ 2200/50/2259b.cpp
@@ -0,0 +1,50 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& v)
+{
+    for (T& a : v)
+        input >> a;
+
+    return input;
+}
+
+void answer(unsigned x)
+{
+    std::cout << x << '\n';
+}
+
+void solve(const std::vector<unsigned>& a)
+{
+    unsigned f[4] = {};
+    for (const auto x : a)
+        ++f[x%4];
+
+    answer(std::max({ f[0], f[1] + f[3], f[2] }));
+}
+
+void test_case()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<unsigned> a(n);
+    std::cin >> a;
+
+    solve(a);
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    size_t t;
+    std::cin >> t;
+
+    while (t-- > 0)
+        test_case();
+
+    return 0;
+}
